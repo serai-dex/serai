854fca3806aff228881f85528d4cc144637e1d3a
Luke Parker <lukeparker5132@gmail.com>
2022-07-09 21:51:39 -0400

    Close https://github.com/serai-dex/serai/issues/30.
    An extremely minimal subset of Monero is now all that's built, and I'm 
    sufficiently happy with it.

--- coins/monero/build.rs
+++ coins/monero/build.rs
@@ -14,7 +14,6 @@ fn main() {
 
   // Use a file to signal if Monero was already built, as that should never be rebuilt
   // If the signaling file was deleted, run this script again to rebuild Monero though
-  // TODO: Move this signaling file into OUT_DIR once Monero is built statically successfully
   println!("cargo:rerun-if-changed=c/.build/monero");
   if !Path::new("c/.build/monero").exists() {
     if !Command::new("make").arg(format!("-j{}", &env::var("THREADS").unwrap_or("2".to_string())))
@@ -28,81 +27,46 @@ fn main() {
     }
   }
 
- println!("cargo:rerun-if-env-changed=OUT_DIR");
- if !Path::new(
-    &format!(
-      "{}/{}cncrypto.{}",
-      out_dir,
-      &env::consts::DLL_PREFIX,
-      &env::consts::DLL_EXTENSION
-    )
-  ).exists() {
-    let mut paths = vec![
-      "c/monero/build/release/contrib/epee/src/libepee.a".to_string(),
-      "c/monero/build/release/external/easylogging++/libeasylogging.a".to_string(),
-      "c/monero/build/release/external/randomx/librandomx.a".to_string()
-    ];
-
-    for (folder, lib) in [
-      ("common", "common"),
-      ("crypto", "cncrypto"),
-      ("crypto/wallet", "wallet-crypto"),
-      ("cryptonote_basic", "cryptonote_basic"),
-      ("cryptonote_basic", "cryptonote_format_utils_basic"),
-      ("", "version"),
-      ("device", "device"),
-      ("ringct", "ringct_basic"),
-      ("ringct", "ringct")
-    ] {
-      paths.push(
-        format!(
-          "c/monero/build/release/src/{}/{}{}.a",
-          folder,
-          &env::consts::DLL_PREFIX,
-          lib
-        )
-      );
-    }
-
-    for path in paths {
-      if !Command::new("cp").args(&[&path, out_dir]).status().unwrap().success() {
-        panic!("Failed to cp {}", path);
-      }
-    }
-  }
-
   println!("cargo:rerun-if-changed=c/wrapper.cpp");
-  if !Path::new(&format!("{}/{}wrapper.a", out_dir, &env::consts::DLL_PREFIX)).exists() {
-    cc::Build::new()
-      .file("c/wrapper.cpp")
-      .cpp(true)
-      .warnings(false)
-      .include("c/monero/contrib/epee/include")
-      .include("c/monero/src")
-      .compile("wrapper");
-  }
+  cc::Build::new()
+    .static_flag(true)
+    .warnings(false)
+    .extra_warnings(false)
+    .flag("-Wno-deprecated-declarations")
+
+    .include("c/monero/external/supercop/include")
+    .include("c/monero/contrib/epee/include")
+    .include("c/monero/src")
+    .include("c/monero/build/release/generated_include")
+
+    .define("AUTO_INITIALIZE_EASYLOGGINGPP", None)
+    .include("c/monero/external/easylogging++")
+    .file("c/monero/external/easylogging++/easylogging++.cc")
+
+    .file("c/monero/src/common/aligned.c")
+    .file("c/monero/src/common/perf_timer.cpp")
+
+    .include("c/monero/src/crypto")
+    .file("c/monero/src/crypto/crypto-ops-data.c")
+    .file("c/monero/src/crypto/crypto-ops.c")
+    .file("c/monero/src/crypto/keccak.c")
+    .file("c/monero/src/crypto/hash.c")
+
+    .include("c/monero/src/device")
+    .file("c/monero/src/device/device_default.cpp")
+
+    .include("c/monero/src/ringct")
+    .file("c/monero/src/ringct/rctCryptoOps.c")
+    .file("c/monero/src/ringct/rctTypes.cpp")
+    .file("c/monero/src/ringct/rctOps.cpp")
+    .file("c/monero/src/ringct/multiexp.cc")
+    .file("c/monero/src/ringct/bulletproofs.cc")
+    .file("c/monero/src/ringct/rctSigs.cpp")
+
+    .file("c/wrapper.cpp")
+    .compile("wrapper");
 
   println!("cargo:rustc-link-search={}", out_dir);
   println!("cargo:rustc-link-lib=wrapper");
-  println!("cargo:rustc-link-lib=ringct");
-  println!("cargo:rustc-link-lib=ringct_basic");
-  println!("cargo:rustc-link-lib=device");
-  println!("cargo:rustc-link-lib=cryptonote_basic");
-  println!("cargo:rustc-link-lib=cncrypto");
-  println!("cargo:rustc-link-lib=cryptonote_format_utils_basic");
-  println!("cargo:rustc-link-lib=version");
-  println!("cargo:rustc-link-lib=wallet-crypto");
-  println!("cargo:rustc-link-lib=easylogging");
-  println!("cargo:rustc-link-lib=epee");
-  println!("cargo:rustc-link-lib=common");
-  println!("cargo:rustc-link-lib=randomx");
-  println!("cargo:rustc-link-lib=unbound");
-  println!("cargo:rustc-link-lib=sodium");
-  println!("cargo:rustc-link-lib=boost_system");
-  println!("cargo:rustc-link-lib=boost_thread");
-  println!("cargo:rustc-link-lib=boost_filesystem");
-  println!("cargo:rustc-link-lib=hidapi-hidraw");
   println!("cargo:rustc-link-lib=stdc++");
-
-  println!("cargo:rustc-link-arg=-zmuldefs");
 }
--- coins/monero/src/lib.rs
+++ coins/monero/src/lib.rs
@@ -1,5 +1,9 @@
+use std::slice;
+
 use lazy_static::lazy_static;
 use rand_core::{RngCore, CryptoRng};
+
+use subtle::ConstantTimeEq;
 
 use tiny_keccak::{Hasher, Keccak};
 
@@ -30,6 +34,29 @@ lazy_static! {
     hex::decode("8b655970153799af2aeadc9ff1add0ea6c7251d54154cfa92c173a0dd39c1f94").unwrap().try_into().unwrap()
   ).decompress().unwrap();
   static ref H_TABLE: EdwardsBasepointTable = EdwardsBasepointTable::create(&*H);
+}
+
+// Function from libsodium our subsection of Monero relies on. Implementing it here means we don't
+// need to link against libsodium
+#[no_mangle]
+unsafe extern "C" fn crypto_verify_32(a: *const u8, b: *const u8) -> isize {
+  isize::from(
+    slice::from_raw_parts(a, 32).ct_eq(slice::from_raw_parts(b, 32)).unwrap_u8()
+  ) - 1
+}
+
+// Offer a wide reduction to C. Our seeded RNG prevented Monero from defining an unbiased scalar
+// generation function, and in order to not use Monero code (which would require propagating its
+// license), the function was rewritten. It was rewritten with wide reduction, instead of rejection
+// sampling however, hence the need for this function
+#[no_mangle]
+unsafe extern "C" fn monero_wide_reduce(value: *mut u8) {
+  let res = Scalar::from_bytes_mod_order_wide(
+    std::slice::from_raw_parts(value, 64).try_into().unwrap()
+  );
+  for (i, b) in res.to_bytes().iter().enumerate() {
+    value.add(i).write(*b);
+  }
 }
 
 #[allow(non_snake_case)]
