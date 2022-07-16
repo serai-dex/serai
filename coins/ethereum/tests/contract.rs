cb8c1f7bb3dc15612154f6b71d39c5bfb007032c
noot <elizabethjbinks@gmail.com>
2022-07-16 17:09:05 -0400

    update ethereum-serai license to GPL and fmt

--- coins/ethereum/tests/contract.rs
+++ coins/ethereum/tests/contract.rs
@@ -4,63 +4,57 @@ use std::{convert::TryFrom, sync::Arc, time::Duration};
 
 #[tokio::test]
 async fn test_deploy_contract() {
-    let anvil = Anvil::new().spawn();
-    let wallet: LocalWallet = anvil.keys()[0].clone().into();
-    let provider = Provider::<Http>::try_from(anvil.endpoint())
-        .unwrap()
-        .interval(Duration::from_millis(10u64));
-    let client = Arc::new(SignerMiddleware::new(provider, wallet));
+  let anvil = Anvil::new().spawn();
+  let wallet: LocalWallet = anvil.keys()[0].clone().into();
+  let provider =
+    Provider::<Http>::try_from(anvil.endpoint()).unwrap().interval(Duration::from_millis(10u64));
+  let client = Arc::new(SignerMiddleware::new(provider, wallet));
 
-    let _contract = deploy_schnorr_verifier_contract(client).await.unwrap();
+  let _contract = deploy_schnorr_verifier_contract(client).await.unwrap();
 }
 
 #[tokio::test]
 async fn test_ecrecover_hack() {
-    use ethereum_serai::crypto;
-    use ethers::utils::keccak256;
-    use frost::{
-        algorithm::Schnorr,
-        curve::Secp256k1,
-        tests::{algorithm_machines, key_gen, sign},
-    };
-    use k256::elliptic_curve::bigint::ArrayEncoding;
-    use k256::{Scalar, U256};
-    use rand::rngs::OsRng;
+  use ethereum_serai::crypto;
+  use ethers::utils::keccak256;
+  use frost::{
+    algorithm::Schnorr,
+    curve::Secp256k1,
+    tests::{algorithm_machines, key_gen, sign},
+  };
+  use k256::elliptic_curve::bigint::ArrayEncoding;
+  use k256::{Scalar, U256};
+  use rand::rngs::OsRng;
 
-    let anvil = Anvil::new().spawn();
-    let wallet: LocalWallet = anvil.keys()[0].clone().into();
-    let provider = Provider::<Http>::try_from(anvil.endpoint())
-        .unwrap()
-        .interval(Duration::from_millis(10u64));
-    let chain_id = provider.get_chainid().await.unwrap();
-    let client = Arc::new(SignerMiddleware::new(provider, wallet));
+  let anvil = Anvil::new().spawn();
+  let wallet: LocalWallet = anvil.keys()[0].clone().into();
+  let provider =
+    Provider::<Http>::try_from(anvil.endpoint()).unwrap().interval(Duration::from_millis(10u64));
+  let chain_id = provider.get_chainid().await.unwrap();
+  let client = Arc::new(SignerMiddleware::new(provider, wallet));
 
-    let keys = key_gen::<_, Secp256k1>(&mut OsRng);
-    let group_key = keys[&1].group_key();
+  let keys = key_gen::<_, Secp256k1>(&mut OsRng);
+  let group_key = keys[&1].group_key();
 
-    const MESSAGE: &'static [u8] = b"Hello, World!";
-    let hashed_message = keccak256(MESSAGE);
-    let chain_id = U256::from(Scalar::from(chain_id.as_u32()));
+  const MESSAGE: &'static [u8] = b"Hello, World!";
+  let hashed_message = keccak256(MESSAGE);
+  let chain_id = U256::from(Scalar::from(chain_id.as_u32()));
 
-    let full_message = &[chain_id.to_be_byte_array().as_slice(), &hashed_message].concat();
+  let full_message = &[chain_id.to_be_byte_array().as_slice(), &hashed_message].concat();
 
-    let sig = sign(
-        &mut OsRng,
-        algorithm_machines(
-            &mut OsRng,
-            Schnorr::<Secp256k1, crypto::EthereumHram>::new(),
-            &keys,
-        ),
-        full_message,
-    );
-    let mut processed_sig =
-        crypto::process_signature_for_contract(hashed_message, &sig.R, sig.s, &group_key, chain_id);
+  let sig = sign(
+    &mut OsRng,
+    algorithm_machines(&mut OsRng, Schnorr::<Secp256k1, crypto::EthereumHram>::new(), &keys),
+    full_message,
+  );
+  let mut processed_sig =
+    crypto::process_signature_for_contract(hashed_message, &sig.R, sig.s, &group_key, chain_id);
 
-    let contract = deploy_schnorr_verifier_contract(client).await.unwrap();
-    call_verify(&contract, &processed_sig).await.unwrap();
+  let contract = deploy_schnorr_verifier_contract(client).await.unwrap();
+  call_verify(&contract, &processed_sig).await.unwrap();
 
-    // test invalid signature fails
-    processed_sig.message[0] = 0;
-    let res = call_verify(&contract, &processed_sig).await;
-    assert!(res.is_err());
+  // test invalid signature fails
+  processed_sig.message[0] = 0;
+  let res = call_verify(&contract, &processed_sig).await;
+  assert!(res.is_err());
 }
--- coins/ethereum/tests/crypto.rs
+++ coins/ethereum/tests/crypto.rs
@@ -1,86 +1,80 @@
 use ethereum_serai::crypto::*;
 use frost::curve::Secp256k1;
 use k256::{
-    elliptic_curve::{bigint::ArrayEncoding, ops::Reduce, sec1::ToEncodedPoint},
-    ProjectivePoint, Scalar, U256,
+  elliptic_curve::{bigint::ArrayEncoding, ops::Reduce, sec1::ToEncodedPoint},
+  ProjectivePoint, Scalar, U256,
 };
 
 #[test]
 fn test_ecrecover() {
-    use k256::ecdsa::{
-        recoverable::Signature,
-        signature::{Signer, Verifier},
-        SigningKey, VerifyingKey,
-    };
-    use rand::rngs::OsRng;
+  use k256::ecdsa::{
+    recoverable::Signature,
+    signature::{Signer, Verifier},
+    SigningKey, VerifyingKey,
+  };
+  use rand::rngs::OsRng;
 
-    let private = SigningKey::random(&mut OsRng);
-    let public = VerifyingKey::from(&private);
+  let private = SigningKey::random(&mut OsRng);
+  let public = VerifyingKey::from(&private);
 
-    const MESSAGE: &'static [u8] = b"Hello, World!";
-    let sig: Signature = private.sign(MESSAGE);
-    public.verify(MESSAGE, &sig).unwrap();
+  const MESSAGE: &'static [u8] = b"Hello, World!";
+  let sig: Signature = private.sign(MESSAGE);
+  public.verify(MESSAGE, &sig).unwrap();
 
-    assert_eq!(
-        ecrecover(
-            hash_to_scalar(MESSAGE),
-            sig.as_ref()[64],
-            *sig.r(),
-            *sig.s()
-        )
-        .unwrap(),
-        address(&ProjectivePoint::from(public))
-    );
+  assert_eq!(
+    ecrecover(hash_to_scalar(MESSAGE), sig.as_ref()[64], *sig.r(), *sig.s()).unwrap(),
+    address(&ProjectivePoint::from(public))
+  );
 }
 
 #[test]
 fn test_signing() {
-    use frost::{
-        algorithm::Schnorr,
-        tests::{algorithm_machines, key_gen, sign},
-    };
-    use rand::rngs::OsRng;
+  use frost::{
+    algorithm::Schnorr,
+    tests::{algorithm_machines, key_gen, sign},
+  };
+  use rand::rngs::OsRng;
 
-    let keys = key_gen::<_, Secp256k1>(&mut OsRng);
-    let _group_key = keys[&1].group_key();
+  let keys = key_gen::<_, Secp256k1>(&mut OsRng);
+  let _group_key = keys[&1].group_key();
 
-    const MESSAGE: &'static [u8] = b"Hello, World!";
+  const MESSAGE: &'static [u8] = b"Hello, World!";
 
-    let _sig = sign(
-        &mut OsRng,
-        algorithm_machines(&mut OsRng, Schnorr::<Secp256k1, EthereumHram>::new(), &keys),
-        MESSAGE,
-    );
+  let _sig = sign(
+    &mut OsRng,
+    algorithm_machines(&mut OsRng, Schnorr::<Secp256k1, EthereumHram>::new(), &keys),
+    MESSAGE,
+  );
 }
 
 #[test]
 fn test_ecrecover_hack() {
-    use frost::{
-        algorithm::Schnorr,
-        tests::{algorithm_machines, key_gen, sign},
-    };
-    use rand::rngs::OsRng;
+  use frost::{
+    algorithm::Schnorr,
+    tests::{algorithm_machines, key_gen, sign},
+  };
+  use rand::rngs::OsRng;
 
-    let keys = key_gen::<_, Secp256k1>(&mut OsRng);
-    let group_key = keys[&1].group_key();
-    let group_key_encoded = group_key.to_encoded_point(true);
-    let group_key_compressed = group_key_encoded.as_ref();
-    let group_key_x = Scalar::from_uint_reduced(U256::from_be_slice(&group_key_compressed[1..33]));
+  let keys = key_gen::<_, Secp256k1>(&mut OsRng);
+  let group_key = keys[&1].group_key();
+  let group_key_encoded = group_key.to_encoded_point(true);
+  let group_key_compressed = group_key_encoded.as_ref();
+  let group_key_x = Scalar::from_uint_reduced(U256::from_be_slice(&group_key_compressed[1 .. 33]));
 
-    const MESSAGE: &'static [u8] = b"Hello, World!";
-    let hashed_message = keccak256(MESSAGE);
-    let chain_id = U256::from(Scalar::ONE);
+  const MESSAGE: &'static [u8] = b"Hello, World!";
+  let hashed_message = keccak256(MESSAGE);
+  let chain_id = U256::from(Scalar::ONE);
 
-    let full_message = &[chain_id.to_be_byte_array().as_slice(), &hashed_message].concat();
+  let full_message = &[chain_id.to_be_byte_array().as_slice(), &hashed_message].concat();
 
-    let sig = sign(
-        &mut OsRng,
-        algorithm_machines(&mut OsRng, Schnorr::<Secp256k1, EthereumHram>::new(), &keys),
-        full_message,
-    );
+  let sig = sign(
+    &mut OsRng,
+    algorithm_machines(&mut OsRng, Schnorr::<Secp256k1, EthereumHram>::new(), &keys),
+    full_message,
+  );
 
-    let (sr, er) =
-        preprocess_signature_for_ecrecover(hashed_message, &sig.R, sig.s, &group_key, chain_id);
-    let q = ecrecover(sr, group_key_compressed[0] - 2, group_key_x, er).unwrap();
-    assert_eq!(q, address(&sig.R));
+  let (sr, er) =
+    preprocess_signature_for_ecrecover(hashed_message, &sig.R, sig.s, &group_key, chain_id);
+  let q = ecrecover(sr, group_key_compressed[0] - 2, group_key_x, er).unwrap();
+  assert_eq!(q, address(&sig.R));
 }
