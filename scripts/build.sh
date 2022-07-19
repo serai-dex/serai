dcdda925bee01180310a975ad5ec30b5679665ac
noot <elizabethjbinks@gmail.com>
2022-07-19 13:30:52 -0400

    install solc-select, use stable rust to build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 BASEDIR=$(pwd)
-rustup install nightly
-rustup default nightly
+rustup default stable
 git submodule update --init --recursive
 #sudo apt-get install libgtest-dev && cd /usr/src/gtest && sudo cmake . && sudo make
 #sudo mv lib/libg* /usr/lib/
 cd ${BASEDIR}
 sudo apt update && sudo apt install build-essential cmake pkg-config libssl-dev libzmq3-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev libpgm-dev qttools5-dev-tools libhidapi-dev libusb-1.0-0-dev libprotobuf-dev protobuf-compiler libudev-dev libboost-chrono-dev libboost-date-time-dev libboost-filesystem-dev libboost-locale-dev libboost-program-options-dev libboost-regex-dev libboost-serialization-dev libboost-system-dev libboost-thread-dev python3 ccache doxygen graphviz libboost-serialization-dev libboost-system-dev libboost-thread-dev python3 ccache doxygen graphviz
+sudo apt install python-pip3
+pip3 install solc-select
+solc-select install 0.8.4
 cargo build --all
