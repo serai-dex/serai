b4f9d9ac0a59c05ffb0f0eb394b7164428e4f111
Luke Parker <lukeparker5132@gmail.com>
2022-07-21 07:53:56 -0400

    Add a mint function capable of executing instructions

--- contracts/asset/lib.rs
+++ contracts/asset/lib.rs
@@ -5,12 +5,22 @@
 pub mod asset {
   use ink_prelude::string::String;
   use ink_storage::traits::SpreadAllocate;
+  use ink_env::{
+    Error, CallFlags,
+    call::{Selector, ExecutionInput, Call, build_call},
+  };
 
   use openbrush::contracts::psp22::*;
   use openbrush::contracts::psp22::extensions::metadata::*;
-  use openbrush::contracts::psp22::extensions::mintable::*;
 
-  use serai_extension::only_inherent::only_inherent;
+  use serai_extension::{SeraiEnvironment, only_inherent::only_inherent};
+
+  struct RawCall<'a>(&'a [u8]);
+  impl<'a> scale::Encode for RawCall<'a> {
+    fn encode_to<T: ?Sized + scale::Output>(&self, dest: &mut T) {
+      dest.write(self.0);
+    }
+  }
 
   #[ink(storage)]
   #[derive(Default, SpreadAllocate, PSP22Storage, PSP22MetadataStorage)]
@@ -24,16 +34,6 @@ pub mod asset {
   impl PSP22 for SeraiAsset {}
   impl PSP22Metadata for SeraiAsset {}
 
-  impl PSP22Mintable for SeraiAsset {
-    #[ink(message)]
-    #[openbrush::modifiers(only_inherent)]
-    fn mint(&mut self, account: AccountId, amount: Balance) -> Result<(), PSP22Error> {
-      // TODO: Check there's room in the bond to mint this
-      // TODO: Transfer and call account on mint
-      self._mint(account, amount)
-    }
-  }
-
   impl SeraiAsset {
     #[ink(constructor)]
     pub fn new(name: String, symbol: String) -> Self {
@@ -42,6 +42,36 @@ pub mod asset {
         instance.metadata.symbol = Some(symbol);
         instance.metadata.decimals = 8;
       })
+    }
+
+    #[ink(message)]
+    #[openbrush::modifiers(only_inherent)]
+    pub fn mint(
+      &mut self,
+      account: AccountId,
+      amount: Balance,
+      instruction: Vec<u8>,
+    ) -> Result<(), PSP22Error> {
+      // TODO: Check there's room in the bond to mint this
+      self._mint(account, amount)?;
+      match build_call::<SeraiEnvironment>()
+        .call_type(Call::new().callee(account))
+        .call_flags(CallFlags::default().set_allow_reentry(true))
+        .exec_input(
+          ExecutionInput::new(Selector::new(
+            instruction[.. 4]
+              .try_into()
+              .map_err(|_| PSP22Error::Custom("invalid in instruction".to_string()))?,
+          ))
+          .push_arg(RawCall(&instruction[4 ..])),
+        )
+        .returns::<()>()
+        .fire()
+      {
+        Ok(_) => Ok(()),
+        Err(Error::NotCallable) => Ok(()),
+        Err(_) => Err(PSP22Error::Custom("invalid in instruction".to_string())),
+      }
     }
   }
 }
