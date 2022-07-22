18142e87055d9b66948978f123ea54e1ee482d93
Luke Parker <lukeparker5132@gmail.com>
2022-07-22 04:24:33 -0400

    Rewrite workflow
    Splits tasks into a more modular structure. Also uses 
    actions-rs/toolchain.

--- scripts/fmt.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rustup component add rustfmt --toolchain nightly-x86_64-unknown-linux-gnu
-cargo +nightly fmt --all
--- scripts/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-./coins/monero/c/monero/build/release/bin/monerod --detach --regtest --offline --fixed-difficulty=1 --rpc-bind-ip 127.0.0.1 --rpc-bind-port 18081
-cargo test --all-features 
