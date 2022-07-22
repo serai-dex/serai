d86ee2b9b8f4c91c4580e37c6beeda240a9b05f9
Luke Parker <lukeparker5132@gmail.com>
2022-07-22 03:30:43 -0400

    Remove try-runtime
    I tried to get this to work for an hour. I have no idea why it doesn't, 
    yet it doesn't.

--- substrate/runtime/src/lib.rs
+++ substrate/runtime/src/lib.rs
@@ -4,7 +4,6 @@
 #[cfg(feature = "std")]
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
-use sp_api::impl_runtime_apis;
 use sp_core::{crypto::KeyTypeId, OpaqueMetadata};
 use sp_runtime::{
   create_runtime_str, generic, impl_opaque_keys,
@@ -280,11 +279,11 @@ construct_runtime!(
     UncheckedExtrinsic = UncheckedExtrinsic
   {
     System: frame_system,
-  RandomnessCollectiveFlip: pallet_randomness_collective_flip,
+    RandomnessCollectiveFlip: pallet_randomness_collective_flip,
     Timestamp: pallet_timestamp,
     Balances: pallet_balances,
     TransactionPayment: pallet_transaction_payment,
-  Contracts: pallet_contracts,
+    Contracts: pallet_contracts,
   }
 );
 
@@ -302,7 +301,7 @@ mod benches {
   );
 }
 
-impl_runtime_apis! {
+sp_api::impl_runtime_apis! {
   impl sp_api::Core<Block> for Runtime {
     fn version() -> RuntimeVersion {
       VERSION
@@ -396,67 +395,6 @@ impl_runtime_apis! {
     }
   }
 
-  #[cfg(feature = "runtime-benchmarks")]
-  impl frame_benchmarking::Benchmark<Block> for Runtime {
-    fn benchmark_metadata(extra: bool) -> (
-      Vec<frame_benchmarking::BenchmarkList>,
-      Vec<frame_support::traits::StorageInfo>,
-    ) {
-      use frame_benchmarking::{baseline, Benchmarking, BenchmarkList};
-      use frame_support::traits::StorageInfoTrait;
-      use frame_system_benchmarking::Pallet as SystemBench;
-      use baseline::Pallet as BaselineBench;
-
-      let mut list = Vec::<BenchmarkList>::new();
-      list_benchmarks!(list, extra);
-
-      let storage_info = AllPalletsWithSystem::storage_info();
-
-      (list, storage_info)
-    }
-
-    fn dispatch_benchmark(
-      config: frame_benchmarking::BenchmarkConfig
-    ) -> Result<Vec<frame_benchmarking::BenchmarkBatch>, sp_runtime::RuntimeString> {
-      use frame_benchmarking::{baseline, Benchmarking, BenchmarkBatch, TrackedStorageKey};
-
-      use frame_system_benchmarking::Pallet as SystemBench;
-      use baseline::Pallet as BaselineBench;
-
-      impl frame_system_benchmarking::Config for Runtime {}
-      impl baseline::Config for Runtime {}
-
-      let whitelist: Vec<TrackedStorageKey> = vec![
-        // Block Number
-        hex_literal::hex!(
-          "26aa394eea5630e07c48ae0c9558cef702a5c1b19ab7a04f536c519aca4983ac"
-        ).to_vec().into(),
-        // Total Issuance
-        hex_literal::hex!(
-          "c2261276cc9d1f8598ea4b6a74b15c2f57c875e4cff74148e4628f264b974c80"
-        ).to_vec().into(),
-        // Execution Phase
-        hex_literal::hex!(
-          "26aa394eea5630e07c48ae0c9558cef7ff553b5a9862a516939d82b3d3d8661a"
-        ).to_vec().into(),
-        // Event Count
-        hex_literal::hex!(
-          "26aa394eea5630e07c48ae0c9558cef70a98fdbe9ce6c55837576c60c7af3850"
-        ).to_vec().into(),
-        // System Events
-        hex_literal::hex!(
-          "26aa394eea5630e07c48ae0c9558cef780d41e5e16056765bc8461851072c9d7"
-        ).to_vec().into(),
-      ];
-
-      let mut batches = Vec::<BenchmarkBatch>::new();
-      let params = (&config, &whitelist);
-      add_benchmarks!(params, batches);
-
-      Ok(batches)
-    }
-  }
-
   impl pallet_contracts_rpc_runtime_api::ContractsApi<Block, AccountId, Balance, BlockNumber, Hash>
       for Runtime
   {
@@ -516,18 +454,64 @@ impl_runtime_apis! {
     }
   }
 
-  #[cfg(feature = "try-runtime")]
-  impl frame_try_runtime::TryRuntime<Block> for Runtime {
-    fn on_runtime_upgrade() -> (Weight, Weight) {
-      // NOTE: intentional unwrap: we don't want to propagate the error backwards, and want to
-      // have a backtrace here. If any of the pre/post migration checks fail, we shall stop
-      // right here and right now.
-      let weight = Executive::try_runtime_upgrade().unwrap();
-      (weight, BlockWeights::get().max_block)
+  #[cfg(feature = "runtime-benchmarks")]
+  impl frame_benchmarking::Benchmark<Block> for Runtime {
+    fn benchmark_metadata(extra: bool) -> (
+      Vec<frame_benchmarking::BenchmarkList>,
+      Vec<frame_support::traits::StorageInfo>,
+    ) {
+      use frame_benchmarking::{baseline, Benchmarking, BenchmarkList};
+      use frame_support::traits::StorageInfoTrait;
+      use frame_system_benchmarking::Pallet as SystemBench;
+      use baseline::Pallet as BaselineBench;
+
+      let mut list = Vec::<BenchmarkList>::new();
+      list_benchmarks!(list, extra);
+
+      let storage_info = AllPalletsWithSystem::storage_info();
+
+      (list, storage_info)
     }
 
-    fn execute_block_no_check(block: Block) -> Weight {
-      Executive::execute_block_no_check(block)
+    fn dispatch_benchmark(
+      config: frame_benchmarking::BenchmarkConfig
+    ) -> Result<Vec<frame_benchmarking::BenchmarkBatch>, sp_runtime::RuntimeString> {
+      use frame_benchmarking::{baseline, Benchmarking, BenchmarkBatch, TrackedStorageKey};
+
+      use frame_system_benchmarking::Pallet as SystemBench;
+      use baseline::Pallet as BaselineBench;
+
+      impl frame_system_benchmarking::Config for Runtime {}
+      impl baseline::Config for Runtime {}
+
+      let whitelist: Vec<TrackedStorageKey> = vec![
+        // Block Number
+        hex_literal::hex!(
+          "26aa394eea5630e07c48ae0c9558cef702a5c1b19ab7a04f536c519aca4983ac"
+        ).to_vec().into(),
+        // Total Issuance
+        hex_literal::hex!(
+          "c2261276cc9d1f8598ea4b6a74b15c2f57c875e4cff74148e4628f264b974c80"
+        ).to_vec().into(),
+        // Execution Phase
+        hex_literal::hex!(
+          "26aa394eea5630e07c48ae0c9558cef7ff553b5a9862a516939d82b3d3d8661a"
+        ).to_vec().into(),
+        // Event Count
+        hex_literal::hex!(
+          "26aa394eea5630e07c48ae0c9558cef70a98fdbe9ce6c55837576c60c7af3850"
+        ).to_vec().into(),
+        // System Events
+        hex_literal::hex!(
+          "26aa394eea5630e07c48ae0c9558cef780d41e5e16056765bc8461851072c9d7"
+        ).to_vec().into(),
+      ];
+
+      let mut batches = Vec::<BenchmarkBatch>::new();
+      let params = (&config, &whitelist);
+      add_benchmarks!(params, batches);
+
+      Ok(batches)
     }
   }
 }
