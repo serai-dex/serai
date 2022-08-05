d71a6305af9d943b35dc43dea13d606240df60d4
noot <elizabethjbinks@gmail.com>
2022-08-05 16:51:29 -0400

    var name changes

--- coins/ethereum/contracts/Router.sol
+++ coins/ethereum/contracts/Router.sol
@@ -22,10 +22,10 @@ contract Router is Schnorr {
         bytes32 message = keccak256(abi.encode(transactions));
         require(verify(parity, px, message, s, e));
         for(uint256 i = 0; i < transactions.length; i++) {
-                (bool success, bytes memory data) = transactions[i].to.call{value: transactions[i].value}(
+                (bool success, bytes memory returndata) = transactions[i].to.call{value: transactions[i].value}(
                     transactions[i].data
                 );
-                emit Executed(success, data);
+                emit Executed(success, returndata);
         }
     }
 
@@ -36,18 +36,18 @@ contract Router is Schnorr {
         bytes32 s,
         bytes32 e
     ) public {
-        uint256 signatureSize = 104; // idk don't actually store this
+        // uint256 signatureSize = 104; // idk don't actually store this
         bytes32 message;
         assembly {
-            calldatacopy(0x40, 0, sub(calldatasize(), signatureSize))
-            message := keccak256(0x40, sub(calldatasize(), signatureSize))
+            calldatacopy(0x40, 0, sub(calldatasize(), 104))
+            message := keccak256(0x40, sub(calldatasize(), 104))
         }
         require(verify(parity, px, message, s, e));
         for(uint256 i = 0; i < transactions.length; i++) {
-                (bool success, bytes memory data) = transactions[i].to.call{value: transactions[i].value}(
+                (bool success, bytes memory returndata) = transactions[i].to.call{value: transactions[i].value}(
                     transactions[i].data
                 );
-                emit Executed(success, data);
+                emit Executed(success, returndata);
         }
     }
 }
