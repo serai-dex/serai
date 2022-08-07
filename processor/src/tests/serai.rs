796338353345d17deea03d1901e91840f8853cf8
Luke Parker <lukeparker5132@gmail.com>
2022-08-07 04:24:20 -0400

    Workaround the delay in the subscription

--- processor/src/tests/serai.rs
+++ processor/src/tests/serai.rs
@@ -10,17 +10,22 @@ use crate::serai::Serai;
 
 #[tokio::test]
 async fn get_events() {
-  let id = Arc::new(Mutex::new(0));
+  let height = Arc::new(Mutex::new(Serai::height().await.unwrap() + 2));
+  let next_id = Arc::new(Mutex::new(0));
 
-  let mut rpc = RpcModule::new(id.clone());
+  let mut rpc = RpcModule::new((height.clone(), next_id.clone()));
   rpc
-    .register_async_method("processor_coins", |_, id| async move {
-      let batch = Batch {
-        id: id.lock().unwrap().clone(),
+    .register_async_method("processor_coins", |_, context| async move {
+      let mut batch = Batch {
+        id: context.1.lock().unwrap().clone(),
         instructions: vec![InInstruction { destination: [0xff; 32], amount: 1, data: vec![] }],
       };
 
       let height = Serai::height().await.unwrap();
+      // Workaround for the delay in the notifications subscription
+      if *context.0.lock().unwrap() < height {
+        batch.id += 1;
+      }
       // Subtract by 2 for now to bypass the delay period in this naive queue
       let pending = PendingBatch { reported_at: height - 2, batch };
 
@@ -41,7 +46,7 @@ async fn get_events() {
   let serai = Serai::new().await;
   let mut batches = serai.batches().await.unwrap();
   loop {
-    dbg!(batches.next().await.unwrap().unwrap());
-    *id.lock().unwrap() += 1;
+    *next_id.lock().unwrap() = dbg!(batches.next().await.unwrap().unwrap()).event.1 + 1;
+    *height.lock().unwrap() = Serai::height().await.unwrap();
   }
 }
