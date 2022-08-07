7b76faa4a57779c97e6ba0e0f015b4002df16b51
Luke Parker <lukeparker5132@gmail.com>
2022-08-07 02:06:06 -0400

    Correct ID handling of the processor test

--- processor/src/tests/serai.rs
+++ processor/src/tests/serai.rs
@@ -1,3 +1,5 @@
+use std::sync::{Arc, Mutex};
+
 use futures::StreamExt;
 
 use in_instructions::{InInstruction, Batch, PendingBatch, Coin};
@@ -8,17 +10,19 @@ use crate::serai::Serai;
 
 #[tokio::test]
 async fn get_events() {
+  let id = Arc::new(Mutex::new(0));
 
-  let mut rpc = RpcModule::new(());
+  let mut rpc = RpcModule::new(id.clone());
   rpc
-    .register_async_method("processor_coins", |_, _| async move {
+    .register_async_method("processor_coins", |_, id| async move {
       let batch = Batch {
-        id: 0,
+        id: id.lock().unwrap().clone(),
         instructions: vec![InInstruction { destination: [0xff; 32], amount: 1, data: vec![] }],
       };
 
       let height = Serai::height().await.unwrap();
-      let pending = PendingBatch { reported_at: height, batch };
+      // Subtract by 2 for now to bypass the delay period in this naive queue
+      let pending = PendingBatch { reported_at: height - 2, batch };
 
       let coin_height = height + 100;
       let coin = Coin { height: coin_height, batches: vec![pending] };
@@ -38,5 +42,6 @@ async fn get_events() {
   let mut batches = serai.batches().await.unwrap();
   loop {
     dbg!(batches.next().await.unwrap().unwrap());
+    *id.lock().unwrap() += 1;
   }
 }
