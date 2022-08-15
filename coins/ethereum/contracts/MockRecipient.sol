15470398f5f5e9dba4f8ee04330303194750b21b
noot <elizabethjbinks@gmail.com>
2022-08-14 22:20:09 -0400

    remove unused files

--- coins/ethereum/contracts/MockRecipient.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: AGPLv3
-pragma solidity ^0.8.0;
-
-contract MockRecipient {
-    function callMe() public payable {
-        payable(msg.sender).transfer(msg.value);
-    }
-}
