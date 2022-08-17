8a79c415ab06ae7d584a184494f30a6cb120507d
Luke Parker <lukeparker5132@gmail.com>
2022-08-16 22:31:24 -0400

    Schnorr signature lib

---
+++ crypto/schnorr/src/lib.rs
@@ -0,0 +1,129 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use core::marker::PhantomData;
+
+use rand_core::{RngCore, CryptoRng};
+
+use curve::{
+  group::{ff::Field, Group},
+  Curve,
+};
+
+#[cfg(feature = "batch")]
+use multiexp::BatchVerifier;
+
+pub trait Signature<C: Curve>: Sized + Clone + Copy {
+  /// Sign a msg with the provided nonce.
+  fn sign_core(key: C::F, nonce: C::F, msg: &[u8]) -> Self;
+
+  /// Sign a msg with a randomly generated nonce.
+  fn sign_random_nonce<R: RngCore + CryptoRng>(rng: &mut R, key: C::F, msg: &[u8]) -> Self {
+    Self::sign_core(key, C::F::random(rng), msg)
+  }
+
+  /*
+  For a properly selected hash function (no length extension attacks and so on), this would be
+  optimal. sign could then call sign_deterministic_nonce with SHA3. The issues are two-fold:
+  1) Needing to perform multiple hashes for the needed bits/use SHAKE256
+  2) Needing to convert this to a Scalar for an arbitratry field (only possible with
+     double-and-add-style schemes)
+  The latter is the main objection at this time.
+  */
+  /// Sign a msg with a deterministic nonce generated as H(x || m).
+  // pub fn sign_deterministic_nonce<D: Digest>(key: C::F, msg: &[u8]) -> Self {}
+
+  /// Alias to the preferred sign function, which is currently sign_random_nonce but may change to
+  /// a deterministic scheme in the future.
+  fn sign<R: RngCore + CryptoRng>(rng: &mut R, key: C::F, msg: &[u8]) -> Self {
+    Self::sign_random_nonce(rng, key, msg)
+  }
+
+  /// Verify a signature.
+  #[must_use]
+  fn verify(&self, key: C::G, msg: &[u8]) -> bool;
+}
+
+/// A parameterizable HRAm to support a variety of signature specifications.
+pub trait Hram<C: Curve>: Sized + Clone + Copy {
+  /// HRAM function to generate a challenge
+  #[allow(non_snake_case)]
+  fn hram(R: C::G, A: C::G, m: &[u8]) -> C::F;
+}
+
+#[allow(non_snake_case)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub struct Schnorr<C: Curve, H: Hram<C>> {
+  pub R: C::G,
+  pub s: C::F,
+  _hram: PhantomData<H>,
+}
+
+#[allow(non_snake_case)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub struct ClassicalSchnorr<C: Curve, H: Hram<C>> {
+  pub c: C::F,
+  pub s: C::F,
+  _hram: PhantomData<H>,
+}
+
+impl<C: Curve, H: Hram<C>> Schnorr<C, H> {
+  #[allow(non_snake_case)]
+  pub fn new(R: C::G, s: C::F) -> Self {
+    Self { R, s, _hram: PhantomData }
+  }
+
+  fn verification_statements(&self, key: C::G, msg: &[u8]) -> [(C::F, C::G); 3] {
+    // R + cA == sG where s = r + cx
+    [(C::F::one(), self.R), (H::hram(self.R, key, msg), key), (-self.s, C::generator())]
+  }
+
+  #[cfg(feature = "batch")]
+  pub fn queue_batch_verification<R: RngCore + CryptoRng, Id: Copy>(
+    &self,
+    rng: &mut R,
+    verifier: &mut BatchVerifier<Id, C::G>,
+    id: Id,
+    key: C::G,
+    msg: &[u8],
+  ) {
+    verifier.queue(rng, id, self.verification_statements(key, msg));
+  }
+}
+
+impl<C: Curve, H: Hram<C>> Signature<C> for Schnorr<C, H> {
+  fn sign_core(key: C::F, nonce: C::F, msg: &[u8]) -> Self {
+    #[allow(non_snake_case)]
+    let R = C::generator() * nonce;
+    Self { R, s: nonce + (key * H::hram(R, C::generator() * key, msg)), _hram: PhantomData }
+  }
+
+  /// Verify a signature.
+  #[must_use]
+  fn verify(&self, key: C::G, msg: &[u8]) -> bool {
+    let mut accum = C::G::identity();
+    for statement in self.verification_statements(key, msg) {
+      accum += statement.1 * statement.0;
+    }
+    accum.is_identity().into()
+  }
+}
+
+impl<C: Curve, H: Hram<C>> ClassicalSchnorr<C, H> {
+  pub fn new(c: C::F, s: C::F) -> Self {
+    Self { c, s, _hram: PhantomData }
+  }
+}
+
+impl<C: Curve, H: Hram<C>> Signature<C> for ClassicalSchnorr<C, H> {
+  fn sign_core(key: C::F, nonce: C::F, msg: &[u8]) -> Self {
+    let c = H::hram(C::generator() * nonce, C::generator() * key, msg);
+    Self { c, s: nonce - (key * c), _hram: PhantomData }
+  }
+
+  /// Verify a signature.
+  #[must_use]
+  fn verify(&self, key: C::G, msg: &[u8]) -> bool {
+    self.c == H::hram((C::generator() * self.s) + (key * self.c), key, msg)
+  }
+}
+
---
+++ crypto/schnorr/tests/tests.rs
@@ -0,0 +1,115 @@
+use rand_core::OsRng;
+
+use blake2::{Digest, Blake2b512};
+
+use curve::group::{ff::Field, Group, GroupEncoding};
+use dalek_ff_group::{Scalar, RistrettoPoint};
+
+use multiexp::BatchVerifier;
+
+use modular_schnorr::{Hram, Signature, Schnorr, ClassicalSchnorr};
+
+const MSG: &[u8] = b"Hello, World";
+
+#[derive(Clone, Copy)]
+struct SimpleHram;
+impl Hram<RistrettoPoint> for SimpleHram {
+  #[allow(non_snake_case)]
+  fn hram(R: RistrettoPoint, A: RistrettoPoint, m: &[u8]) -> Scalar {
+    Scalar::from_hash(Blake2b512::new().chain_update(&[&R.to_bytes(), &A.to_bytes(), m].concat()))
+  }
+}
+
+#[test]
+fn sign_verify() {
+  let key = Scalar::random(&mut OsRng);
+  assert!(Schnorr::<RistrettoPoint, SimpleHram>::sign(&mut OsRng, key, MSG)
+    .verify(RistrettoPoint::generator() * key, MSG));
+  assert!(ClassicalSchnorr::<RistrettoPoint, SimpleHram>::sign(&mut OsRng, key, MSG)
+    .verify(RistrettoPoint::generator() * key, MSG));
+}
+
+/*
+#[test]
+fn conversion() {
+  let key = Scalar::random(&mut OsRng);
+  let pub = RistrettoPoint::generator() * key;
+  let sig = Schnorr::<RistrettoPoint, SimpleHram>::sign(&mut OsRng, key, MSG);
+  assert!(sig.verify(public_key, MSG));
+
+  let classical = ClassicalSchnorr::from(sig);
+  assert!(classical.verify(public_key, MSG));
+  let back = Schnorr::from(classical);
+  assert!(back.verify(public_key, MSG));
+  assert_eq!(sig, back);
+}
+*/
+
+#[test]
+fn zero() {
+  // True zero should pass for any message
+  assert!(Schnorr::<RistrettoPoint, SimpleHram>::new(RistrettoPoint::identity(), Scalar::zero())
+    .verify(RistrettoPoint::identity(), MSG));
+  // ClassicalSchnorr doesn't have a "true zero" as c is a hash output
+  // While it's possible to craft a c for a 0 nonce, that's just checking a 0 nonce
+
+  // But not for an actual key
+  assert!(!Schnorr::<RistrettoPoint, SimpleHram>::new(RistrettoPoint::identity(), Scalar::zero())
+    .verify(RistrettoPoint::random(&mut OsRng), MSG));
+  assert!(!ClassicalSchnorr::<RistrettoPoint, SimpleHram>::new(Scalar::zero(), Scalar::zero())
+    .verify(RistrettoPoint::random(&mut OsRng), MSG));
+}
+
+#[test]
+fn random_fails() {
+  assert!(!Schnorr::<RistrettoPoint, SimpleHram>::new(
+    RistrettoPoint::random(&mut OsRng),
+    Scalar::random(&mut OsRng)
+  )
+  .verify(RistrettoPoint::random(&mut OsRng), MSG));
+
+  assert!(!ClassicalSchnorr::<RistrettoPoint, SimpleHram>::new(
+    Scalar::random(&mut OsRng),
+    Scalar::random(&mut OsRng)
+  )
+  .verify(RistrettoPoint::random(&mut OsRng), MSG));
+}
+
+#[cfg(feature = "batch")]
+#[test]
+fn batch_verify() {
+  // Create 5 signatures
+  let mut keys = vec![];
+  let mut sigs = vec![];
+  let mut verifier = BatchVerifier::new(5);
+  for i in 0 .. 5 {
+    keys.push(Scalar::random(&mut OsRng));
+    sigs.push(Schnorr::<RistrettoPoint, SimpleHram>::sign(
+      &mut OsRng,
+      keys[i],
+      &[MSG, &i.to_le_bytes()].concat(),
+    ));
+    sigs[i].queue_batch_verification(
+      &mut OsRng,
+      &mut verifier,
+      i,
+      RistrettoPoint::generator() * keys[i],
+      &[MSG, &i.to_le_bytes()].concat(),
+    );
+  }
+  assert!(verifier.verify_vartime());
+
+  // Test invalid signatures don't batch verify
+  Schnorr::<RistrettoPoint, SimpleHram>::new(
+    RistrettoPoint::random(&mut OsRng),
+    Scalar::random(&mut OsRng),
+  )
+  .queue_batch_verification(
+    &mut OsRng,
+    &mut verifier,
+    5,
+    RistrettoPoint::random(&mut OsRng),
+    MSG,
+  );
+  assert_eq!(verifier.verify_vartime_with_vartime_blame().unwrap_err(), 5);
+}
