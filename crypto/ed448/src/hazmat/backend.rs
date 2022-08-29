a3a224b1e12822c2e79920999eddede8dfa6bbaf
Luke Parker <lukeparker5132@gmail.com>
2022-08-29 02:08:22 -0400

    Remove the variable time backend
    With the recent performance improvements, the constant time backend is 
    now 4x faster than the variable time backend was. While the variable 
    time backend remains much faster, and the constant time backend is still 
    slow compared to other libraries, it's sufficiently performant now.
    
    The FROST test, which runs a series of multiexps over the curve, does 
    take 218.26s while Ristretto takes 1 and secp256k1 takes 4.57s.
    
    While 50x slower than secp256k1 is horrible, it's ~1.5 orders of 
    magntiude, which is close enough to the desire stated in 
    https://github.com/serai-dex/serai/issues/108 to meet it.
    
    Largely makes this library safe to use.

--- crypto/ed448/src/hazmat/backend.rs
+++ crypto/ed448/src/hazmat/backend.rs
@@ -1,23 +1,9 @@
-// Constant time if told to AND this isn't a test with variable time (--all-features)
-#[cfg(all(not(all(feature = "variable-time", test)), feature = "constant-time"))]
 pub(crate) mod constant_time;
 
-// Variable time if told to AND (this isn't constant time OR this is a test)
-#[cfg(all(any(not(feature = "constant-time"), test), feature = "variable-time"))]
-pub(crate) mod variable_time;
-
 pub mod scalar {
-  #[cfg(all(not(all(feature = "variable-time", test)), feature = "constant-time"))]
   pub use crate::hazmat::backend::constant_time::scalar::*;
-
-  #[cfg(all(any(not(feature = "constant-time"), test), feature = "variable-time"))]
-  pub use crate::hazmat::backend::variable_time::scalar::*;
 }
 
 pub mod field {
-  #[cfg(all(not(all(feature = "variable-time", test)), feature = "constant-time"))]
   pub use crate::hazmat::backend::constant_time::field::*;
-
-  #[cfg(all(any(not(feature = "constant-time"), test), feature = "variable-time"))]
-  pub use crate::hazmat::backend::variable_time::field::*;
 }
