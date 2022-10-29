9eb5df218aad4b701a3b32da56771b2b948186d8
Luke Parker <lukeparker5132@gmail.com>
2022-10-29 04:10:33 -0400

    Update processor

--- processor/src/lib.rs
+++ processor/src/lib.rs
@@ -3,7 +3,10 @@ use std::{marker::Send, collections::HashMap};
 use async_trait::async_trait;
 use thiserror::Error;
 
-use frost::{curve::Curve, FrostError};
+use frost::{
+  curve::{Ciphersuite, Curve},
+  FrostError,
+};
 
 mod coin;
 use coin::{CoinError, Coin};
@@ -35,6 +38,6 @@ pub enum SignError {
 // Doesn't consider the current group key to increase the simplicity of verifying Serai's status
 // Takes an index, k, to support protocols which use multiple secondary keys
 // Presumably a view key
-pub(crate) fn additional_key<C: Coin>(k: u64) -> <C::Curve as Curve>::F {
+pub(crate) fn additional_key<C: Coin>(k: u64) -> <C::Curve as Ciphersuite>::F {
   C::Curve::hash_to_F(b"Serai DEX Additional Key", &[C::ID, &k.to_le_bytes()].concat())
 }
