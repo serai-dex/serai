cdc2025391bb1c3d7ef881d5527ac765b3388e00
Luke Parker <lukeparker5132@gmail.com>
2022-11-02 23:31:19 -0400

    Correct Dave, Eve, and Ferdie to not run as validators

--- deploy/serai/scripts/entry-dev.sh
+++ deploy/serai/scripts/entry-dev.sh
@@ -2,6 +2,7 @@
 
 if [[ -z $VALIDATOR ]]; then
 <<<<<<< HEAD
+<<<<<<< HEAD
   serai-node --tmp --chain $CHAIN --name $NAME
 else
   serai-node --tmp --chain $CHAIN --$NAME
@@ -10,4 +11,9 @@ else
 else
     serai-node --chain $CHAIN --$NAME --validator
 >>>>>>> e3fc3f28 (Configure node for a multi-node testnet)
+=======
+    serai-node --chain $CHAIN --name $NAME
+else
+    serai-node --chain $CHAIN --$NAME
+>>>>>>> 131355b1 (Correct Dave, Eve, and Ferdie to not run as validators)
 fi
