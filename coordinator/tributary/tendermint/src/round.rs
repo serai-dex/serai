6331c5fc2b1ca0304ac778e13ca70e7be82004d3
Luke Parker <lukeparker5132@gmail.com>
2022-11-12 10:41:09 -0500

    BlockData and RoundData structs

--- coordinator/tributary/tendermint/src/round.rs
+++ coordinator/tributary/tendermint/src/round.rs
@@ -10,22 +10,37 @@ use tokio::time::sleep;
 use crate::{
   time::CanonicalInstant,
   Step,
+<<<<<<< HEAD:coordinator/tributary/tendermint/src/round.rs
   ext::{RoundNumber, Network},
+=======
+  ext::{Round, Network},
+>>>>>>> 9e72f873 (BlockData and RoundData structs):substrate/tendermint/machine/src/round.rs
 };
 
 pub(crate) struct RoundData<N: Network> {
   _network: PhantomData<N>,
+<<<<<<< HEAD:coordinator/tributary/tendermint/src/round.rs
   pub(crate) number: RoundNumber,
+=======
+  pub(crate) round: Round,
+>>>>>>> 9e72f873 (BlockData and RoundData structs):substrate/tendermint/machine/src/round.rs
   pub(crate) start_time: CanonicalInstant,
   pub(crate) step: Step,
   pub(crate) timeouts: HashMap<Step, Instant>,
 }
 
 impl<N: Network> RoundData<N> {
+<<<<<<< HEAD:coordinator/tributary/tendermint/src/round.rs
   pub(crate) fn new(number: RoundNumber, start_time: CanonicalInstant) -> Self {
     RoundData {
       _network: PhantomData,
       number,
+=======
+  pub(crate) fn new(round: Round, start_time: CanonicalInstant) -> Self {
+    RoundData {
+      _network: PhantomData,
+      round,
+>>>>>>> 9e72f873 (BlockData and RoundData structs):substrate/tendermint/machine/src/round.rs
       start_time,
       step: Step::Propose,
       timeouts: HashMap::new(),
@@ -33,8 +48,13 @@ impl<N: Network> RoundData<N> {
   }
 
   fn timeout(&self, step: Step) -> CanonicalInstant {
+<<<<<<< HEAD:coordinator/tributary/tendermint/src/round.rs
     let adjusted_block = N::BLOCK_PROCESSING_TIME * (self.number.0 + 1);
     let adjusted_latency = N::LATENCY_TIME * (self.number.0 + 1);
+=======
+    let adjusted_block = N::BLOCK_PROCESSING_TIME * (self.round.0 + 1);
+    let adjusted_latency = N::LATENCY_TIME * (self.round.0 + 1);
+>>>>>>> 9e72f873 (BlockData and RoundData structs):substrate/tendermint/machine/src/round.rs
     let offset = Duration::from_secs(
       (match step {
         Step::Propose => adjusted_block + adjusted_latency,
@@ -55,7 +75,10 @@ impl<N: Network> RoundData<N> {
     self.timeouts.entry(step).or_insert(timeout);
   }
 
+<<<<<<< HEAD:coordinator/tributary/tendermint/src/round.rs
   // Poll all set timeouts, returning the Step whose timeout has just expired
+=======
+>>>>>>> 9e72f873 (BlockData and RoundData structs):substrate/tendermint/machine/src/round.rs
   pub(crate) async fn timeout_future(&self) -> Step {
     let timeout_future = |step| {
       let timeout = self.timeouts.get(&step).copied();
