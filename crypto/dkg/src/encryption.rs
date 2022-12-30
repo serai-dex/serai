cabdfb9138c549b5543630fc3f17e84583403c5c
Luke Parker <lukeparker5132@gmail.com>
2022-12-30 02:49:53 -0500

    Rewrite DKG encryption to enable per-message decryption without side effects
    This isn't technically true as I already know a break in this which I'll
    correct for shortly.
    
    Does update documentation to explain the new scheme. Required for blame.

--- crypto/dkg/src/encryption.rs
+++ crypto/dkg/src/encryption.rs
@@ -13,11 +13,12 @@ use chacha20::{
   Key as Cc20Key, Nonce as Cc20Iv, ChaCha20,
 };
 
-use group::GroupEncoding;
+use transcript::{Transcript, RecommendedTranscript};
 
+use group::GroupEncoding;
 use ciphersuite::Ciphersuite;
 
-use transcript::{Transcript, RecommendedTranscript};
+use dleq::DLEqProof;
 
 use crate::ThresholdParams;
 
@@ -35,6 +36,7 @@ pub trait ReadWrite: Sized {
 pub trait Message: Clone + PartialEq + Eq + Debug + Zeroize + ReadWrite {}
 impl<M: Clone + PartialEq + Eq + Debug + Zeroize + ReadWrite> Message for M {}
 
+/// Wraps a message with a key to use for encryption in the future.
 #[derive(Clone, PartialEq, Eq, Debug, Zeroize)]
 pub struct EncryptionKeyMessage<C: Ciphersuite, M: Message> {
   msg: M,
@@ -61,16 +63,23 @@ impl<C: Ciphersuite, M: Message> EncryptionKeyMessage<C, M> {
 
 pub trait Encryptable: Clone + AsMut<[u8]> + Zeroize + ReadWrite {}
 impl<E: Clone + AsMut<[u8]> + Zeroize + ReadWrite> Encryptable for E {}
-#[derive(Clone, Zeroize)]
-pub struct EncryptedMessage<E: Encryptable>(Zeroizing<E>);
 
-impl<E: Encryptable> EncryptedMessage<E> {
+/// An encrypted message, with a per-message encryption key enabling revealing specific messages
+/// without side effects.
+#[derive(Clone, Zeroize)]
+pub struct EncryptedMessage<C: Ciphersuite, E: Encryptable> {
+  key: C::G,
+  msg: Zeroizing<E>,
+}
+
+impl<C: Ciphersuite, E: Encryptable> EncryptedMessage<C, E> {
   pub fn read<R: Read>(reader: &mut R, params: ThresholdParams) -> io::Result<Self> {
-    Ok(Self(Zeroizing::new(E::read(reader, params)?)))
+    Ok(Self { key: C::read_G(reader)?, msg: Zeroizing::new(E::read(reader, params)?) })
   }
 
   pub fn write<W: Write>(&self, writer: &mut W) -> io::Result<()> {
-    self.0.write(writer)
+    writer.write_all(self.key.to_bytes().as_ref())?;
+    self.msg.write(writer)
   }
 
   pub fn serialize(&self) -> Vec<u8> {
@@ -80,6 +89,39 @@ impl<E: Encryptable> EncryptedMessage<E> {
   }
 }
 
+/// A proof that the provided point is the legitimately derived shared key for some message.
+#[derive(Clone, PartialEq, Eq, Debug, Zeroize)]
+pub struct EncryptionKeyProof<C: Ciphersuite> {
+  key: Zeroizing<C::G>,
+  dleq: DLEqProof<C::G>,
+}
+
+impl<C: Ciphersuite> EncryptionKeyProof<C> {
+  pub fn read<R: Read>(reader: &mut R) -> io::Result<Self> {
+    Ok(Self { key: Zeroizing::new(C::read_G(reader)?), dleq: DLEqProof::read(reader)? })
+  }
+
+  pub fn write<W: Write>(&self, writer: &mut W) -> io::Result<()> {
+    writer.write_all(self.key.to_bytes().as_ref())?;
+    self.dleq.write(writer)
+  }
+
+  pub fn serialize(&self) -> Vec<u8> {
+    let mut buf = vec![];
+    self.write(&mut buf).unwrap();
+    buf
+  }
+}
+
+fn ecdh<C: Ciphersuite>(private: &Zeroizing<C::F>, public: C::G) -> Zeroizing<C::G> {
+  Zeroizing::new(public * private.deref())
+}
+
+fn encryption_key_transcript() -> RecommendedTranscript {
+  RecommendedTranscript::new(b"DKG Encryption Key Proof v0.2")
+}
+
+// A simple box for managing encryption.
 #[derive(Clone)]
 pub(crate) struct Encryption<Id: Eq + Hash, C: Ciphersuite> {
   dst: &'static [u8],
@@ -120,24 +162,15 @@ impl<Id: Eq + Hash, C: Ciphersuite> Encryption<Id, C> {
     msg.msg
   }
 
-  fn cipher(&self, participant: Id, encrypt: bool) -> ChaCha20 {
+  fn cipher(&self, ecdh: &Zeroizing<C::G>) -> ChaCha20 {
     // Ideally, we'd box this transcript with ZAlloc, yet that's only possible on nightly
     // TODO: https://github.com/serai-dex/serai/issues/151
     let mut transcript = RecommendedTranscript::new(b"DKG Encryption v0.2");
     transcript.domain_separate(self.dst);
 
-    let other = self.enc_keys[&participant];
-    if encrypt {
-      transcript.append_message(b"sender", self.enc_pub_key.to_bytes());
-      transcript.append_message(b"receiver", other.to_bytes());
-    } else {
-      transcript.append_message(b"sender", other.to_bytes());
-      transcript.append_message(b"receiver", self.enc_pub_key.to_bytes());
-    }
-
-    let mut shared = Zeroizing::new(other * self.enc_key.deref()).deref().to_bytes();
-    transcript.append_message(b"shared_key", shared.as_ref());
-    shared.as_mut().zeroize();
+    let mut ecdh = ecdh.to_bytes();
+    transcript.append_message(b"shared_key", ecdh.as_ref());
+    ecdh.as_mut().zeroize();
 
     let zeroize = |buf: &mut [u8]| buf.zeroize();
 
@@ -161,21 +194,70 @@ impl<Id: Eq + Hash, C: Ciphersuite> Encryption<Id, C> {
     res
   }
 
-  pub(crate) fn encrypt<E: Encryptable>(
+  pub(crate) fn encrypt<R: RngCore + CryptoRng, E: Encryptable>(
     &self,
+    rng: &mut R,
     participant: Id,
     mut msg: Zeroizing<E>,
-  ) -> EncryptedMessage<E> {
-    self.cipher(participant, true).apply_keystream(msg.as_mut().as_mut());
-    EncryptedMessage(msg)
+  ) -> EncryptedMessage<C, E> {
+    /*
+    The following code could be used to replace the requirement on an RNG here.
+    It's just currently not an issue to require taking in an RNG here.
+    let last = self.last_enc_key.to_bytes();
+    self.last_enc_key = C::hash_to_F(b"encryption_base", last.as_ref());
+    let key = C::hash_to_F(b"encryption_key", last.as_ref());
+    last.as_mut().zeroize();
+    */
+
+    let key = Zeroizing::new(C::random_nonzero_F(rng));
+    self
+      .cipher(&ecdh::<C>(&key, self.enc_keys[&participant]))
+      .apply_keystream(msg.as_mut().as_mut());
+    EncryptedMessage { key: C::generator() * key.deref(), msg }
   }
 
-  pub(crate) fn decrypt<E: Encryptable>(
+  pub(crate) fn decrypt<R: RngCore + CryptoRng, E: Encryptable>(
     &self,
-    participant: Id,
-    mut msg: EncryptedMessage<E>,
-  ) -> Zeroizing<E> {
-    self.cipher(participant, false).apply_keystream(msg.0.as_mut().as_mut());
-    msg.0
+    rng: &mut R,
+    mut msg: EncryptedMessage<C, E>,
+  ) -> (Zeroizing<E>, EncryptionKeyProof<C>) {
+    let key = ecdh::<C>(&self.enc_key, msg.key);
+    self.cipher(&key).apply_keystream(msg.msg.as_mut().as_mut());
+    (
+      msg.msg,
+      EncryptionKeyProof {
+        key,
+        dleq: DLEqProof::prove(
+          rng,
+          &mut encryption_key_transcript(),
+          &[C::generator(), msg.key],
+          &self.enc_key,
+        ),
+      },
+    )
+  }
+
+  // Given a message, and the intended decryptor, and a proof for its key, decrypt the message.
+  // Returns None if the key was wrong.
+  pub(crate) fn decrypt_with_proof<E: Encryptable>(
+    &self,
+    decryptor: Id,
+    mut msg: EncryptedMessage<C, E>,
+    proof: EncryptionKeyProof<C>,
+  ) -> Option<Zeroizing<E>> {
+    // Verify this is the decryption key for this message
+    if proof
+      .dleq
+      .verify(
+        &mut encryption_key_transcript(),
+        &[C::generator(), msg.key],
+        &[self.enc_keys[&decryptor], *proof.key],
+      )
+      .is_err()
+    {
+      return None;
+    }
+    self.cipher(&proof.key).apply_keystream(msg.msg.as_mut().as_mut());
+    Some(msg.msg)
   }
 }
