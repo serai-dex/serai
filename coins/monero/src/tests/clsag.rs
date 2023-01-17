e5c98e5b198afcf6a07615ced513bd958888a589
Luke Parker <lukeparker5132@gmail.com>
2023-01-17 17:53:09 -0500

    fmt

--- coins/monero/src/tests/clsag.rs
+++ coins/monero/src/tests/clsag.rs
@@ -107,11 +107,7 @@ fn clsag_multisig() {
     Arc::new(RwLock::new(Some(ClsagDetails::new(
       ClsagInput::new(
         Commitment::new(randomness, AMOUNT),
-        Decoys {
-          i: RING_INDEX,
-          offsets: (1 ..= RING_LEN).collect(),
-          ring: ring.clone(),
-        },
+        Decoys { i: RING_INDEX, offsets: (1 ..= RING_LEN).collect(), ring: ring.clone() },
       )
       .unwrap(),
       mask_sum,
