f9569dc56b21ba383bc23a26589b97b538cd9bef
Luke Parker <lukeparker5132@gmail.com>
2023-01-29 19:17:53 -0500

    Finish merging develop

---
+++ processor/src/tests/bitcoin.rs
@@ -0,0 +1,11 @@
+use crate::{
+  coin::{Coin, Bitcoin},
+  tests::test_send,
+};
+
+#[tokio::test]
+async fn bitcoin() {
+  let bitcoin = Bitcoin::new("http://serai:seraidex@127.0.0.1:18443".to_string()).await;
+  let fee = bitcoin.get_fee().await;
+  test_send(bitcoin, fee).await;
+}
--- processor/src/tests/mod.rs
+++ processor/src/tests/mod.rs
@@ -1,13 +1,5 @@
 mod send;
 pub(crate) use send::test_send;
 
-/*
-#[tokio::test]
-async fn bitcoin() {
-  let bitcoin = Bitcoin::new("http://serai:seraidex@127.0.0.1:18443".to_string()).await;
-  let fee = bitcoin.get_fee().await;
-  test_send(bitcoin, fee).await;
-}
-*/
-
+mod bitcoin;
 mod monero;
