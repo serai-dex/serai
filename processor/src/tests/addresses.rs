a722930996a513a18a8e890f31f1d360b6a8fd26
Luke Parker <lukeparker5132@gmail.com>
2023-02-19 02:47:54 -0500

    Also test spending the change output

--- processor/src/tests/addresses.rs
+++ processor/src/tests/addresses.rs
@@ -3,14 +3,60 @@ use std::collections::HashMap;
 
 use rand_core::OsRng;
 
+use frost::ThresholdKeys;
+
 use tokio::time::timeout;
 
 use crate::{
-  Plan,
+  Plan, Db,
   coins::{OutputType, Output, Block, Coin},
-  scanner::{ScannerOrder, ScannerEvent, Scanner},
+  scanner::{ScannerOrder, ScannerEvent, Scanner, ScannerHandle},
   tests::{util::db::MemDb, sign},
 };
+
+async fn spend<C: Coin, D: Db>(
+  coin: &C,
+  keys: &HashMap<u16, ThresholdKeys<C::Curve>>,
+  scanner: &mut ScannerHandle<C, D>,
+  outputs: Vec<C::Output>,
+) -> Vec<C::Output> {
+  let key = keys[&1].group_key();
+
+  let mut keys_txs = HashMap::new();
+  for (i, keys) in keys {
+    keys_txs.insert(
+      *i,
+      (
+        keys.clone(),
+        coin
+          .prepare_send(
+            keys.clone(),
+            coin.get_latest_block_number().await.unwrap() - C::CONFIRMATIONS,
+            // Send to a change output
+            Plan { key, inputs: outputs.clone(), payments: vec![], change: Some(key) },
+            coin.get_fee().await,
+          )
+          .await
+          .unwrap()
+          .0,
+      ),
+    );
+  }
+  sign(coin.clone(), keys_txs).await;
+
+  for _ in 0 .. C::CONFIRMATIONS {
+    coin.mine_block().await;
+  }
+  match timeout(Duration::from_secs(10), scanner.events.recv()).await.unwrap().unwrap() {
+    ScannerEvent::Outputs(this_key, _, outputs) => {
+      assert_eq!(this_key, key);
+      assert_eq!(outputs.len(), 1);
+      // Make sure this is actually a change output
+      assert_eq!(outputs[0].kind(), OutputType::Change);
+      outputs
+    }
+  }
+}
 
 pub async fn test_addresses<C: Coin>(coin: C) {
   let mut keys = frost::tests::key_gen::<_, C::Curve>(&mut OsRng);
@@ -50,36 +96,8 @@ pub async fn test_addresses<C: Coin>(coin: C) {
       }
     };
 
-  // Trigger a send with change and ensure we actually get change
-  let mut keys_txs = HashMap::new();
-  for (i, keys) in keys {
-    keys_txs.insert(
-      i,
-      (
-        keys.clone(),
-        coin
-          .prepare_send(
-            keys,
-            coin.get_latest_block_number().await.unwrap() - C::CONFIRMATIONS,
-            Plan { key, inputs: outputs.clone(), payments: vec![], change: Some(key) },
-            coin.get_fee().await,
-          )
-          .await
-          .unwrap()
-          .0,
-      ),
-    );
-  }
-  sign(coin.clone(), keys_txs).await;
-
-  for _ in 0 .. C::CONFIRMATIONS {
-    coin.mine_block().await;
-  }
-  match timeout(Duration::from_secs(10), scanner.events.recv()).await.unwrap().unwrap() {
-    ScannerEvent::Outputs(this_key, _, outputs) => {
-      assert_eq!(this_key, key);
-      assert_eq!(outputs.len(), 1);
-      assert_eq!(outputs[0].kind(), OutputType::Change);
-    }
-  };
+  // Spend the branch output, creating a change output and ensuring we actually get change
+  let outputs = spend(&coin, &keys, &mut scanner, outputs).await;
+  // Also test spending the change output
+  spend(&coin, &keys, &mut scanner, outputs).await;
 }
