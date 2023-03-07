af5702fccd6a846fc8458e917c216c55bce3dd80
Luke Parker <lukeparker5132@gmail.com>
2023-03-07 03:37:30 -0500

    Make encryption public
    It's necessary in order to read encryption messages over the network.

--- crypto/dkg/src/lib.rs
+++ crypto/dkg/src/lib.rs
@@ -23,7 +23,8 @@ use group::{
 
 use ciphersuite::Ciphersuite;
 
-mod encryption;
+/// Encryption types and utilities used to secure DKG messages.
+pub mod encryption;
 
 /// The distributed key generation protocol described in the
 /// [FROST paper](https://eprint.iacr.org/2020/852).
