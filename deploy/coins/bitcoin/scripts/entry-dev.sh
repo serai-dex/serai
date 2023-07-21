298d1fd3badee1a17cd5030d570f8db7da9567cb
Luke Parker <lukeparker5132@gmail.com>
2023-07-21 04:27:14 -0400

    Slim coins and processor Dockerfiles

--- deploy/coins/bitcoin/scripts/entry-dev.sh
+++ deploy/coins/bitcoin/scripts/entry-dev.sh
@@ -5,4 +5,4 @@ RPC_PASS="${RPC_PASS:=seraidex}"
 
 bitcoind -txindex -regtest \
   -rpcuser=$RPC_USER -rpcpassword=$RPC_PASS \
-  -rpcbind=127.0.0.1 -rpcbind=$(hostname) -rpcallowip=0.0.0.0/0
+  -rpcbind=0.0.0.0 -rpcallowip=0.0.0.0/0
