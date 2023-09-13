66a734df7b1b729ac5f76186c0afadde45ca39f4
Luke Parker <lukeparker5132@gmail.com>
2023-09-13 11:40:50 -0400

    Use mini to prove a race condition in the current multisig rotation docs, and prove safety of alternatives
    Technically, the prior commit had mini prove the race condition.
    
    The docs currently say the activation block of the new multisig is the block
    after the next Batch's. If the two next Batches had already entered the
    mempool, prior to set_keys being called, the second next Batch would be
    expected to contain the new key's data yet fail to as the key wasn't public
    when the Batch was actually created.
    
    The naive solution is to create a Batch, publish it, wait until it's included,
    and only then scan the next block. This sets a bound of
    `Batch publication time < block time`. Optimistically, we can publish a Batch
    in 24s while our shortest block time is 2m. Accordingly, we should be fine with
    the naive solution which doesn't take advantage of throughput. #333 may
    significantly change latency however and require an algorithm whose throughput
    exceeds the rate of blocks created.
    
    In order to re-introduce parallelization, enabling throughput, we need to
    define a safe range of blocks to scan without Serai ordering the first one.
    mini demonstrates safety of scanning n blocks Serai hasn't acknowledged, so
    long as the first is scanned before block n+1 is (shifting the n-block window).
    
    The docs will be updated next, to reflect this.

--- mini/src/lib.rs
+++ mini/src/lib.rs
@@ -2,7 +2,7 @@ use std::sync::{Arc as StdArc, RwLock as StdRwLock};
 
 use loom::{
   thread::{self, JoinHandle},
-  sync::{Arc, RwLock},
+  sync::{Arc, RwLock, mpsc},
 };
 
 #[cfg(test)]
@@ -17,52 +17,85 @@ pub struct Batch {
 #[derive(Clone, PartialEq, Eq, Hash, Debug)]
 pub enum Event {
   IncludedBatch(Batch),
+  // Allows if let else on this without clippy believing it's redundant
+  __Ignore,
 }
+
+const BATCH_FTL: u64 = 3;
 
 #[derive(Debug)]
 pub struct Serai {
   handle: JoinHandle<()>,
-  pub active_keys: Arc<RwLock<Vec<u64>>>,
+  remaining_ticks: Arc<RwLock<usize>>,
+  // Activation block, ID
+  pub active_keys: Arc<RwLock<Vec<(u64, u64)>>>,
   pub mempool_batches: Arc<RwLock<Vec<Batch>>>,
-  pub events: StdArc<StdRwLock<Vec<Event>>>,
+  pub events: mpsc::Receiver<Event>,
+  all_events_unsafe: StdArc<StdRwLock<Vec<Event>>>,
 }
 
 impl Serai {
   #[allow(clippy::new_without_default)]
   pub fn new(ticks: usize, mut queued_key: bool) -> Serai {
-    let active_keys = Arc::new(RwLock::new(vec![0]));
+    let remaining_ticks = Arc::new(RwLock::new(ticks));
+
+    let active_keys = Arc::new(RwLock::new(vec![(0, 0)]));
     let mempool_batches = Arc::new(RwLock::new(vec![]));
-    let events = StdArc::new(StdRwLock::new(vec![]));
+    let (events_sender, events_receiver) = mpsc::channel();
+    let all_events_unsafe = StdArc::new(StdRwLock::new(vec![]));
 
     let handle = thread::spawn({
-      let events = events.clone();
+      let remaining_ticks = remaining_ticks.clone();
+
       let active_keys = active_keys.clone();
       let mempool_batches = mempool_batches.clone();
+      let all_events_unsafe = all_events_unsafe.clone();
+
       move || {
-        for _ in 0 .. ticks {
+        while {
+          let mut remaining_ticks = remaining_ticks.write().unwrap();
+          let ticking = *remaining_ticks != 0;
+          *remaining_ticks = remaining_ticks.saturating_sub(1);
+          ticking
+        } {
           let mut batches = mempool_batches.write().unwrap();
           if !batches.is_empty() {
-            let batch = batches.remove(0);
-            events.write().unwrap().push(Event::IncludedBatch(batch));
-            // Activate keys after the next received block
+            let batch: Batch = batches.remove(0);
+
+            // Activate keys after the FTL
             if queued_key {
               let mut active_keys = active_keys.write().unwrap();
               let len = active_keys.len().try_into().unwrap();
-              active_keys.push(len);
+              active_keys.push((batch.block + BATCH_FTL, len));
             }
             queued_key = false;
+
+            let event = Event::IncludedBatch(batch);
+            events_sender.send(event.clone()).unwrap();
+            all_events_unsafe.write().unwrap().push(event);
           }
         }
       }
     });
 
-    Serai { handle, mempool_batches, active_keys, events }
+    Serai {
+      handle,
+      remaining_ticks,
+      mempool_batches,
+      active_keys,
+      events: events_receiver,
+      all_events_unsafe,
+    }
+  }
+
+  pub fn exhausted(&self) -> bool {
+    *self.remaining_ticks.read().unwrap() == 0
   }
 
   pub fn join(self) -> Vec<Event> {
     self.handle.join().unwrap();
 
-    self.events.read().unwrap().clone()
+    self.all_events_unsafe.read().unwrap().clone()
   }
 }
 
@@ -72,14 +105,30 @@ pub struct Processor {
 }
 
 impl Processor {
-  pub fn new(serai: Serai, batches: u64) -> Processor {
+  pub fn new(serai: Serai, blocks: u64) -> Processor {
     let handle = thread::spawn(move || {
-      for b in 0 .. batches {
-        serai
-          .mempool_batches
-          .write()
-          .unwrap()
-          .push(Batch { block: b, keys: serai.active_keys.read().unwrap().clone() });
+      let mut last_known_serai_time = 0;
+      for b in 0 .. blocks {
+        // If this block is too far ahead of Serai's last block, wait for Serai to process
+        while b >= (last_known_serai_time + BATCH_FTL) {
+          if serai.exhausted() {
+            return serai;
+          }
+          let Ok(event) = serai.events.recv() else { return serai };
+          if let Event::IncludedBatch(Batch { block, .. }) = event {
+            last_known_serai_time = block;
+          }
+        }
+        serai.mempool_batches.write().unwrap().push(Batch {
+          block: b,
+          keys: serai
+            .active_keys
+            .read()
+            .unwrap()
+            .iter()
+            .filter_map(|(activation_block, id)| Some(*id).filter(|_| b >= *activation_block))
+            .collect(),
+        });
       }
       serai
     });
--- mini/src/tests/activation_race/mod.rs
+++ mini/src/tests/activation_race/mod.rs
@@ -7,13 +7,43 @@ use crate::*;
 
 #[test]
 fn activation_race() {
+  #[derive(Debug)]
+  struct EagerProcessor {
+    handle: JoinHandle<Serai>,
+  }
+
+  impl EagerProcessor {
+    fn new(serai: Serai, batches: u64) -> EagerProcessor {
+      let handle = thread::spawn(move || {
+        for b in 0 .. batches {
+          serai.mempool_batches.write().unwrap().push(Batch {
+            block: b,
+            keys: serai
+              .active_keys
+              .read()
+              .unwrap()
+              .iter()
+              .filter_map(|(activation_block, id)| Some(*id).filter(|_| b >= *activation_block))
+              .collect(),
+          });
+        }
+        serai
+      });
+      EagerProcessor { handle }
+    }
+
+    fn join(self) -> Serai {
+      self.handle.join().unwrap()
+    }
+  }
+
   let results = StdArc::new(StdRwLock::new(HashSet::new()));
 
   loom::model({
     let results = results.clone();
     move || {
-      let serai = Serai::new(2, true);
-      let processor = Processor::new(serai, 2);
+      let serai = Serai::new(4, true);
+      let processor = EagerProcessor::new(serai, 4);
       let serai = processor.join();
       let events = serai.join();
 
@@ -21,19 +51,124 @@ fn activation_race() {
     }
   });
 
-  assert_eq!(
-    *results.read().unwrap(),
-    HashSet::from([
-      vec![],
-      vec![Event::IncludedBatch(Batch { block: 0, keys: vec![0] })],
-      vec![
-        Event::IncludedBatch(Batch { block: 0, keys: vec![0] }),
-        Event::IncludedBatch(Batch { block: 1, keys: vec![0] })
-      ],
-      vec![
-        Event::IncludedBatch(Batch { block: 0, keys: vec![0] }),
-        Event::IncludedBatch(Batch { block: 1, keys: vec![0, 1] })
-      ],
-    ])
-  );
+  let results: HashSet<_> = results.read().unwrap().clone();
+  assert_eq!(results.len(), 6);
+  for result in results {
+    for (b, batch) in result.into_iter().enumerate() {
+      if b < 3 {
+        assert_eq!(
+          batch,
+          Event::IncludedBatch(Batch { block: b.try_into().unwrap(), keys: vec![0] })
+        );
+      } else {
+        let Event::IncludedBatch(batch) = batch else { panic!("unexpected event") };
+        assert_eq!(batch.block, b.try_into().unwrap());
+        assert!((batch.keys == vec![0]) || (batch.keys == vec![0, 1]));
+      }
+    }
+  }
+}
+
+#[test]
+fn sequential_solves_activation_race() {
+  #[derive(Debug)]
+  struct DelayedProcessor {
+    handle: JoinHandle<Serai>,
+  }
+
+  impl DelayedProcessor {
+    fn new(serai: Serai, batches: u64) -> DelayedProcessor {
+      let handle = thread::spawn(move || {
+        for b in 0 .. batches {
+          let batch = {
+            let mut batches = serai.mempool_batches.write().unwrap();
+            let batch = Batch {
+              block: b,
+              keys: serai
+                .active_keys
+                .read()
+                .unwrap()
+                .iter()
+                .filter_map(|(activation_block, id)| Some(*id).filter(|_| b >= *activation_block))
+                .collect(),
+            };
+            batches.push(batch.clone());
+            batch
+          };
+
+          while (!serai.exhausted()) &&
+            (serai.events.recv().unwrap() != Event::IncludedBatch(batch.clone()))
+          {
+            loom::thread::yield_now();
+          }
+        }
+        serai
+      });
+      DelayedProcessor { handle }
+    }
+
+    fn join(self) -> Serai {
+      self.handle.join().unwrap()
+    }
+  }
+
+  let results = StdArc::new(StdRwLock::new(HashSet::new()));
+
+  loom::model({
+    let results = results.clone();
+    move || {
+      let serai = Serai::new(4, true);
+      let processor = DelayedProcessor::new(serai, 4);
+      let serai = processor.join();
+      let events = serai.join();
+
+      results.write().unwrap().insert(events);
+    }
+  });
+
+  let results: HashSet<_> = results.read().unwrap().clone();
+  assert_eq!(results.len(), 5);
+  for result in results {
+    for (b, batch) in result.into_iter().enumerate() {
+      assert_eq!(
+        batch,
+        Event::IncludedBatch(Batch {
+          block: b.try_into().unwrap(),
+          keys: if b < 3 { vec![0] } else { vec![0, 1] }
+        }),
+      );
+    }
+  }
+}
+
+#[test]
+fn ftl_solves_activation_race() {
+  let results = StdArc::new(StdRwLock::new(HashSet::new()));
+
+  loom::model({
+    let results = results.clone();
+    move || {
+      let serai = Serai::new(4, true);
+      // Uses Processor since this Processor has this algorithm implemented
+      let processor = Processor::new(serai, 4);
+      let serai = processor.join();
+      let events = serai.join();
+
+      results.write().unwrap().insert(events);
+    }
+  });
+
+  let results: HashSet<_> = results.read().unwrap().clone();
+  assert_eq!(results.len(), 5);
+  for result in results {
+    for (b, batch) in result.into_iter().enumerate() {
+      assert_eq!(
+        batch,
+        Event::IncludedBatch(Batch {
+          block: b.try_into().unwrap(),
+          keys: if b < 3 { vec![0] } else { vec![0, 1] }
+        }),
+      );
+    }
+  }
 }
