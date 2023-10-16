cad0d14a0f34a578cf1337fd9a64817c025eaad8
akildemir <34187742+akildemir@users.noreply.github.com>
2023-10-16 13:12:15 +0300

    Update FeeAccount test

--- substrate/coins/primitives/src/lib.rs
+++ substrate/coins/primitives/src/lib.rs
@@ -44,5 +44,5 @@ pub enum Destination {
 #[test]
 fn address() {
   use sp_runtime::traits::TrailingZeroInput;
-  assert_eq!(FEE_ACCOUNT, SeraiAddress::decode(&mut TrailingZeroInput::new(b"Coins")).unwrap());
+  assert_eq!(FEE_ACCOUNT, SeraiAddress::decode(&mut TrailingZeroInput::new(b"FeeAccount")).unwrap());
 }
