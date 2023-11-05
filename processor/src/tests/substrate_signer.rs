ea367a175b0c38466fb0dad3f6ab47d7849053f6
Luke Parker <lukeparker5132@gmail.com>
2023-11-05 09:03:34 -0500

    Correct other rebase artifact

--- processor/src/tests/substrate_signer.rs
+++ processor/src/tests/substrate_signer.rs
@@ -42,7 +42,7 @@ async fn test_substrate_signer() {
         balance: Balance { coin: Coin::Bitcoin, amount: Amount(1000) },
       },
       InInstructionWithBalance {
-        instruction: InInstruction::Dex(DexCall::AddLiquidity(SeraiAddress([0xbb; 32]))),
+        instruction: InInstruction::Dex(DexCall::SwapAndAddLiquidity(SeraiAddress([0xbb; 32]))),
         balance: Balance { coin: Coin::Monero, amount: Amount(9999999999999999) },
       },
     ],
