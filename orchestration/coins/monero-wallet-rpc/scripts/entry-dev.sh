30603438d7dd52fecb247821cf3333cf2a48bd63
rlking <kingcastle@gmx.net>
2024-01-24 23:27:25 +0100

    make monero wallet rpc docker accessible from outside

--- orchestration/coins/monero-wallet-rpc/scripts/entry-dev.sh
+++ orchestration/coins/monero-wallet-rpc/scripts/entry-dev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-monero-wallet-rpc --disable-rpc-login --rpc-bind-port 6061 --allow-mismatched-daemon-version --wallet-dir /home/monero
+monero-wallet-rpc --disable-rpc-login --rpc-bind-port 6061 --rpc-bind-ip=0.0.0.0 --confirm-external-bind --allow-mismatched-daemon-version --wallet-dir /home/monero
