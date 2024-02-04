7468898c81931881cfd863e353a9e2363d9e653d
Luke Parker <lukeparker5132@gmail.com>
2024-02-04 14:00:15 -0500

    Bitcoin and Monero testnet scripts

--- orchestration/testnet/coins/bitcoin/scripts/run.sh
+++ orchestration/testnet/coins/bitcoin/scripts/run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-exit 1
+RPC_USER="${RPC_USER:=serai}"
+RPC_PASS="${RPC_PASS:=seraidex}"
+
+bitcoind -txindex -testnet -port=8333 \
+  -rpcuser=$RPC_USER -rpcpassword=$RPC_PASS \
+  -rpcbind=0.0.0.0 -rpcallowip=0.0.0.0/0 -rpcport=8332
--- orchestration/testnet/coins/monero/scripts/run.sh
+++ orchestration/testnet/coins/monero/scripts/run.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-exit 1
+RPC_USER="${RPC_USER:=serai}"
+RPC_PASS="${RPC_PASS:=seraidex}"
+
+# Run Monero
+monerod --non-interactive --testnet \
+  --no-zmq --rpc-bind-ip=0.0.0.0 --rpc-bind-port=18081 --confirm-external-bind \
+  --rpc-access-control-origins "*" --disable-rpc-ban \
+  --rpc-login=$RPC_USER:$RPC_PASS
