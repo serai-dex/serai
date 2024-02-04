a7e61559e3b1e879982cb865bfccfb4fca997e3f
Luke Parker <lukeparker5132@gmail.com>
2024-02-04 15:45:11 -0500

    Have monero-wallet-rpc specify the monerod login

--- orchestration/dev/coins/monero-wallet-rpc/run.sh
+++ orchestration/dev/coins/monero-wallet-rpc/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 monero-wallet-rpc \
-  --disable-rpc-login \
-  --rpc-bind-ip=0.0.0.0 --confirm-external-bind \
-  --daemon-address monero:18081 --allow-mismatched-daemon-version \
+  --allow-mismatched-daemon-version \
+  --daemon-address monero:18081 --daemon-login serai:seraidex \
+  --disable-rpc-login --rpc-bind-ip=0.0.0.0 --rpc-bind-port 18082 --confirm-external-bind \
   --wallet-dir /home/monero
