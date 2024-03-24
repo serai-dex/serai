9a2d155af24eec6e5755f02de650c7831f982b76
Luke Parker <lukeparker5132@gmail.com>
2024-03-23 20:11:52 -0400

    Add logs to authority discovery

--- orchestration/src/serai.rs
+++ orchestration/src/serai.rs
@@ -15,7 +15,10 @@ pub fn serai(
   let setup_fast_epoch =
     mimalloc(Os::Debian).to_string() + &build_serai_service(true, "fast-epoch", "serai-node");
 
-  let env_vars = [("KEY", hex::encode(serai_key.to_repr()))];
+  let env_vars = [
+    ("KEY", hex::encode(serai_key.to_repr())),
+    ("RUST_LOG", "info,sub-authority-discovery=debug".to_string()),
+  ];
   let mut env_vars_str = String::new();
   for (env_var, value) in env_vars {
     env_vars_str += &format!(r#"{env_var}=${{{env_var}:="{value}"}} "#);
