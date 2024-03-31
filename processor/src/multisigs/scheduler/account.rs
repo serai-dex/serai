357574168abd8f3000fb90ba3d610a11462a60c9
Luke Parker <lukeparker5132@gmail.com>
2024-03-31 04:23:53 -0400

    Add nonce uses, key rotation to the account scheduler

--- processor/src/multisigs/scheduler/account.rs
+++ processor/src/multisigs/scheduler/account.rs
@@ -1,18 +1,31 @@
 use std::{io, collections::HashSet};
 
-use ciphersuite::Ciphersuite;
+use ciphersuite::{group::GroupEncoding, Ciphersuite};
 
 use serai_client::primitives::{NetworkId, Coin, Balance};
 
 use crate::{
   networks::{Output, Network},
-  Db, Payment, Plan,
+  Get, DbTxn, Db, Payment, Plan, create_db,
 };
 
 #[derive(PartialEq, Eq, Debug)]
 pub struct Scheduler<N: Network>(<N::Curve as Ciphersuite>::G, HashSet<Coin>);
 
-impl<N: Network> crate::multisigs::scheduler::Scheduler<N> for Scheduler<N> {
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub(crate) struct Nonce(pub u64);
+
+create_db! {
+  SchedulerDb {
+    LastNonce: () -> u64,
+    UpdatedKey: (key: &[u8]) -> (),
+  }
+}
+
+impl<N: Network> crate::multisigs::scheduler::Scheduler<N> for Scheduler<N>
+where
+  N::Output: From<Nonce>,
+{
   /// Check if this Scheduler is empty.
   fn empty(&self) -> bool {
     true
@@ -42,20 +55,41 @@ impl<N: Network> crate::multisigs::scheduler::Scheduler<N> for Scheduler<N> {
 
   fn schedule<D: Db>(
     &mut self,
-    _txn: &mut D::Transaction<'_>,
+    txn: &mut D::Transaction<'_>,
     utxos: Vec<N::Output>,
     payments: Vec<Payment<N>>,
-    _key_for_any_change: <N::Curve as Ciphersuite>::G,
-    _force_spend: bool,
+    key_for_any_change: <N::Curve as Ciphersuite>::G,
+    force_spend: bool,
   ) -> Vec<Plan<N>> {
     for utxo in utxos {
       assert!(self.1.contains(&utxo.balance().coin));
     }
 
+    let mut nonce = LastNonce::get(txn).map_or(0, |nonce| nonce + 1);
     let mut plans = vec![];
     for chunk in payments.as_slice().chunks(N::MAX_OUTPUTS) {
-      plans.push(Plan { key: self.0, inputs: vec![], payments: chunk.to_vec(), change: None });
+      plans.push(Plan {
+        key: self.0,
+        inputs: vec![N::Output::from(Nonce(nonce))],
+        payments: chunk.to_vec(),
+        change: None,
+      });
+      nonce += 1;
     }
+    LastNonce::set(txn, &nonce);
+
+    // If we're supposed to rotate to the new key, create an empty Plan which will signify the key
+    // update
+    if force_spend && UpdatedKey::get(txn, self.0.to_bytes().as_ref()).is_none() {
+      plans.push(Plan {
+        key: self.0,
+        inputs: vec![],
+        payments: vec![],
+        change: Some(N::external_address(key_for_any_change)),
+      });
+      UpdatedKey::set(txn, self.0.to_bytes().as_ref(), &());
+    }
+
     plans
   }
 
