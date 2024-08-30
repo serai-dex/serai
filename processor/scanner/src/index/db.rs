7283a53b09d3199708b3631c750d06ec22771135
Luke Parker <lukeparker5132@gmail.com>
2024-08-30 00:11:31 -0400

    Remove unused ID -> number lookup

--- processor/scanner/src/index/db.rs
+++ processor/scanner/src/index/db.rs
@@ -4,8 +4,6 @@ create_db!(
   ScannerIndex {
     // A lookup of a block's number to its ID
     BlockId: (number: u64) -> [u8; 32],
-    // A lookup of a block's ID to its number
-    BlockNumber: (id: [u8; 32]) -> u64,
 
     // The latest finalized block to appear on the blockchain
     LatestFinalizedBlock: () -> u64,
@@ -16,13 +14,9 @@ pub(crate) struct IndexDb;
 impl IndexDb {
   pub(crate) fn set_block(txn: &mut impl DbTxn, number: u64, id: [u8; 32]) {
     BlockId::set(txn, number, &id);
-    BlockNumber::set(txn, id, &number);
   }
   pub(crate) fn block_id(getter: &impl Get, number: u64) -> Option<[u8; 32]> {
     BlockId::get(getter, number)
-  }
-  pub(crate) fn block_number(getter: &impl Get, id: [u8; 32]) -> Option<u64> {
-    BlockNumber::get(getter, id)
   }
 
   pub(crate) fn set_latest_finalized_block(txn: &mut impl DbTxn, latest_finalized_block: u64) {
