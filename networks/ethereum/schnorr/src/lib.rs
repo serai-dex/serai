f004c8726f94633413d913f29d5a80327822b021
Luke Parker <lukeparker5132@gmail.com>
2025-01-27 15:38:44 -0500

    Remove unused library bytecode from ethereum-schnorr-contract

--- networks/ethereum/schnorr/src/lib.rs
+++ networks/ethereum/schnorr/src/lib.rs
@@ -3,18 +3,6 @@
 #![deny(missing_docs)]
 #![allow(non_snake_case)]
 
-/// The initialization bytecode of the Schnorr library.
-pub const BYTECODE: &[u8] = {
-  const BYTECODE_HEX: &[u8] =
-    include_bytes!(concat!(env!("OUT_DIR"), "/ethereum-schnorr-contract/Schnorr.bin"));
-  const BYTECODE: [u8; BYTECODE_HEX.len() / 2] =
-    match const_hex::const_decode_to_array::<{ BYTECODE_HEX.len() / 2 }>(BYTECODE_HEX) {
-      Ok(bytecode) => bytecode,
-      Err(_) => panic!("Schnorr.bin did not contain valid hex"),
-    };
-  &BYTECODE
-};
-
 mod public_key;
 pub use public_key::PublicKey;
 mod signature;
