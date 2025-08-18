29400ff41589922766c2a04d4ae7d280c88218e7
Luke Parker <lukeparker5132@gmail.com>
2025-08-18 06:39:39 -0400

    Don't offset nonces yet negate to achieve an even Y coordinate
    Replaces an iterative loop with an immediate result, if action is necessary.

--- networks/bitcoin/tests/wallet.rs
+++ networks/bitcoin/tests/wallet.rs
@@ -80,7 +80,7 @@ async fn send_and_get_output(rpc: &Rpc, scanner: &Scanner, key: ProjectivePoint)
 fn keys() -> (HashMap<Participant, ThresholdKeys<Secp256k1>>, ProjectivePoint) {
   let mut keys = key_gen(&mut OsRng);
   for keys in keys.values_mut() {
-    *keys = tweak_keys(keys);
+    *keys = tweak_keys(keys.clone());
   }
   let key = keys.values().next().unwrap().group_key();
   (keys, key)
