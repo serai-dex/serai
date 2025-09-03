215e41fdb6c63f451bd6bdaef17a42a5f1ca2763
Luke Parker <lukeparker5132@gmail.com>
2025-09-03 06:49:44 -0400

    Remove deprecated APIs from `dalek-ff-group`
    For backwards compatibility, we now use as a patch (as prior done with
    `ciphersuite`).
    
    Removes `crypto-bigint 0.5` from the tree and shapes up what the next release
    will look like.

--- crypto/dalek-ff-group/src/lib.rs
+++ crypto/dalek-ff-group/src/lib.rs
@@ -494,31 +494,3 @@ prime_field::odd_prime_field_with_specific_repr!(
   false,
   crate::ThirtyTwoArray
 );
-
-impl FieldElement {
-  /// Create a FieldElement from a `crypto_bigint::U256`.
-  ///
-  /// This will reduce the `U256` by the modulus, into a member of the field.
-  #[deprecated]
-  pub const fn from_u256(u256: &crypto_bigint_05::U256) -> Self {
-    const MODULUS: crypto_bigint::U256 = crypto_bigint::U256::from_be_hex(
-      "7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffed",
-    );
-    let mut u256 = crypto_bigint::U256::from_words(*u256.as_words());
-    loop {
-      let result = FieldElement::from_bytes(&u256.to_le_bytes());
-      if let Some(result) = result {
-        return result;
-      }
-      u256 = u256.wrapping_sub(&MODULUS);
-    }
-  }
-
-  /// Create a `FieldElement` from the reduction of a 512-bit number.
-  ///
-  /// The bytes are interpreted in little-endian format.
-  #[deprecated]
-  pub fn wide_reduce(value: [u8; 64]) -> Self {
-    <FieldElement as ::ciphersuite::group::ff::FromUniformBytes<_>>::from_uniform_bytes(&value)
-  }
-}
