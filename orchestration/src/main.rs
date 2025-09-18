10c126ad924c438bd8ce04b6099053c6e24d4a89
Luke Parker <lukeparker5132@gmail.com>
2025-09-18 16:05:30 -0400

    Misc updates

--- orchestration/src/main.rs
+++ orchestration/src/main.rs
@@ -149,7 +149,7 @@ fn build_serai_service(prelude: &str, release: bool, features: &str, package: &s
 
   format!(
     r#"
-FROM rust:1.89-slim-bookworm AS builder
+FROM rust:1.90-slim-bookworm AS builder
 
 COPY --from=mimalloc-debian libmimalloc.so /usr/lib
 RUN echo "/usr/lib/libmimalloc.so" >> /etc/ld.so.preload
