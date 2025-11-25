8d8e8a7a779ef272487c4c88cac37b82363067fb
Luke Parker <lukeparker5132@gmail.com>
2025-11-25 16:59:12 -0500

    Remove unnecessary MSRVs from `patches/`

--- patches/option-ext/src/lib.rs
+++ patches/option-ext/src/lib.rs
@@ -1,8 +1,8 @@
-pub trait OptionExt<T: PartialEq> {
-  fn contains(&self, x: &T) -> bool;
+pub trait OptionExt<T> {
+  fn contains(&self, x: &T) -> bool where T: PartialEq;
 }
-impl<T: PartialEq> OptionExt<T> for Option<T> {
-  fn contains(&self, x: &T) -> bool {
+impl<T> OptionExt<T> for Option<T> {
+  fn contains(&self, x: &T) -> bool where T: PartialEq {
     self.as_ref() == Some(x)
   }
 }
