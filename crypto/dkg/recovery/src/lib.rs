c9e693d53524ccee8f2c7b2636dba089db17f262
Luke Parker <lukeparker5132@gmail.com>
2026-05-06 03:30:40 -0400

    Add `dkg_recovery::recover_singular_key`
    This checks for every possible pemutation of the set of signing keys, each is
    able to recover the expected key.
    
    The `permutation` function is reasonable (and tested), but as
    `recover_singular_key` evaluates a permutation, it has factorial complexity and
    really is just for testing purposes (like 3-of-5 DKGs).

--- crypto/dkg/recovery/src/lib.rs
+++ crypto/dkg/recovery/src/lib.rs
@@ -4,7 +4,7 @@
 
 use core::ops::{Deref as _, DerefMut as _};
 extern crate alloc;
-use alloc::vec::Vec;
+use alloc::{vec::Vec, vec};
 
 use zeroize::Zeroizing;
 
@@ -12,8 +12,7 @@ use ciphersuite::{GroupIo, Id};
 
 pub use dkg::*;
 
-/// Errors encountered when recovering a secret-shared key from a collection of
-/// `dkg::ThresholdKeys`.
+/// Errors encountered when recovering a secret-shared key`.
 #[derive(Clone, PartialEq, Eq, Debug, thiserror::Error)]
 pub enum RecoveryError {
   /// No keys were provided.
@@ -33,7 +32,11 @@ pub enum RecoveryError {
   DkgError(DkgError),
 }
 
-/// Recover a shared secret from a collection of `dkg::ThresholdKeys`.
+/// Recover a shared secret from a collection of [`dkg::ThresholdKeys`].
+///
+/// This function assumes `keys` contains no entries for which
+/// `keys_i.params().i() == keys_j.params().i()`. It may spuriously error (with incorrect
+/// description) when such keys are passed to it.
 pub fn recover_key<C: GroupIo + Id>(
   keys: &[ThresholdKeys<C>],
 ) -> Result<Zeroizing<C::F>, RecoveryError> {
@@ -42,12 +45,14 @@ pub fn recover_key<C: GroupIo + Id>(
   let keys_len = keys.len();
   let mut keys = keys.iter();
   let first_keys = keys.next().ok_or(RecoveryError::NoKeysProvided)?;
+
   {
     let t = first_keys.params().t();
     if keys_len < usize::from(t) {
       Err(RecoveryError::NotEnoughKeysProvided { required: t, provided: keys_len })?;
     }
   }
+
   {
     let first_params = (
       first_keys.params().t(),
@@ -82,4 +87,186 @@ pub fn recover_key<C: GroupIo + Id>(
   }
 
   Ok(res)
+}
+
+/// Increment a list representing all possible permutations of a search space.
+///
+/// This is intended for permutating a set of size `<= slots.len()` and accordingly uses `None` to
+/// represent no selected value. `None` is the only value which may appear multiple times. For the
+/// values which are `Some`, their values will be unique and the result will be sorted.
+///
+/// This returns `false` if the permutations have been exhausted, as defined by having already
+/// yielded every unique set for which every member is _less than_ `Some(limit)`. In that case,
+/// `slots` is left as an undefined value.
+#[must_use]
+fn permutation(slots: &mut [Option<u16>], limit: u16) -> bool {
+  let Some(mut i) = slots.len().checked_sub(1) else { return false };
+  if limit == 0 {
+    return false;
+  }
+
+  // Increment the latest index we can
+  loop {
+    let slots_len = slots.len();
+    let Some(slot_value) = &mut slots[i] else {
+      slots[i] = Some(0);
+      break;
+    };
+
+    if let Some(increment) = slot_value.checked_add(1) {
+      // If this index is at its limit, retry with the prior index
+      let final_increment =
+        increment.saturating_add(u16::try_from(slots_len - 1 - i).unwrap_or(u16::MAX));
+      if final_increment >= limit {
+        if i == 0 {
+          return false;
+        }
+        i -= 1;
+        continue;
+      }
+
+      // Assign this slot to its increment
+      *slot_value = increment;
+      break;
+    }
+  }
+
+  // For the slot we incremented, initialize the following slots
+  {
+    let mut assigned_value = slots[i].expect("didn't assign a value in the above permutation loop");
+    for slot in slots.iter_mut().skip(i + 1) {
+      // This was checked when we checked `final_increment`
+      let incremented = assigned_value + 1;
+      assigned_value = incremented;
+      *slot = Some(assigned_value);
+    }
+    if assigned_value >= limit {
+      return false;
+    }
+  }
+
+  // Check this permutation is in the expected form
+  for i in 1 .. slots.len() {
+    debug_assert!(slots[i - 1] <= slots[i]);
+    if slots[i - 1].is_some() {
+      debug_assert!(slots[i - 1] < slots[i]);
+    }
+  }
+  // Check the limit is still respected
+  debug_assert!(slots.last() <= Some(&Some(limit)));
+
+  true
+}
+
+/// Recover a shared secret from a collection of [`dkg::ThresholdKeys`], ensuring the result is
+/// singular.
+///
+/// This performs a recovery for every possible permutation of the input keys, and accordingly has
+/// factorial complexity. It's intended for ensuring that any set of shares from a DKG as usable as
+/// intended. It SHOULD NOT be used for large set sizes.
+///
+/// This function assumes `keys` contains no entries for which
+/// `keys_i.params().i() == keys_j.params().i()`. It may spuriously error (with incorrect
+/// description) when such keys are passed to it.
+pub fn recover_singular_key<C: GroupIo + Id>(
+  keys: &[ThresholdKeys<C>],
+) -> Result<Zeroizing<C::F>, RecoveryError> {
+  let t_present = keys.len();
+  let t_necessary = keys.first().ok_or(RecoveryError::NoKeysProvided)?.params().t();
+
+  let mut to_remove = vec![
+    None;
+    t_present.checked_sub(usize::from(t_necessary)).ok_or(
+      RecoveryError::NotEnoughKeysProvided { required: t_necessary, provided: t_present }
+    )?
+  ];
+  let mut recovered = None;
+  while {
+    let mut keys = keys.to_vec();
+    for to_remove in to_remove.iter().flatten() {
+      keys.remove(usize::from(*to_remove));
+    }
+    let recovered_i = recover_key(&keys)?;
+    if recovered.is_none() {
+      recovered = Some(recovered_i.clone());
+    }
+    if recovered != Some(recovered_i) {
+      Err(RecoveryError::Failure)?;
+    }
+
+    u16::try_from(to_remove.len()).map(|limit| permutation(&mut to_remove, limit)).unwrap_or(false)
+  } {}
+  Ok(recovered.unwrap())
+}
+
+#[test]
+fn test_permutation() {
+  // An empty set cannot be permutated
+  assert!(!permutation(&mut [], 1));
+  // When the limit is the first value incremented to, there are not further permutations
+  assert!(!permutation(&mut [None], 0));
+
+  {
+    let mut set = [None];
+
+    assert!(permutation(&mut set, 2));
+    assert_eq!(set, [Some(0)]);
+    assert!(permutation(&mut set, 2));
+    assert_eq!(set, [Some(1)]);
+
+    assert!(!permutation(&mut set, 2));
+  }
+
+  {
+    let mut set = [None, None];
+
+    assert!(permutation(&mut set, 2));
+    assert_eq!(set, [None, Some(0)]);
+    assert!(permutation(&mut set, 2));
+    assert_eq!(set, [None, Some(1)]);
+    assert!(permutation(&mut set, 2));
+    assert_eq!(set, [Some(0), Some(1)]);
+
+    assert!(!permutation(&mut set, 2));
+  }
+
+  {
+    let mut set = [None, None, None];
+
+    assert!(permutation(&mut set, 4));
+    assert_eq!(set, [None, None, Some(0)]);
+    assert!(permutation(&mut set, 4));
+    assert_eq!(set, [None, None, Some(1)]);
+    assert!(permutation(&mut set, 4));
+    assert_eq!(set, [None, None, Some(2)]);
+    assert!(permutation(&mut set, 4));
+    assert_eq!(set, [None, None, Some(3)]);
+
+    assert!(permutation(&mut set, 4));
+    assert_eq!(set, [None, Some(0), Some(1)]);
+    assert!(permutation(&mut set, 4));
+    assert_eq!(set, [None, Some(0), Some(2)]);
+    assert!(permutation(&mut set, 4));
+    assert_eq!(set, [None, Some(0), Some(3)]);
+
+    assert!(permutation(&mut set, 4));
+    assert_eq!(set, [None, Some(1), Some(2)]);
+    assert!(permutation(&mut set, 4));
+    assert_eq!(set, [None, Some(1), Some(3)]);
+
+    assert!(permutation(&mut set, 4));
+    assert_eq!(set, [None, Some(2), Some(3)]);
+
+    assert!(permutation(&mut set, 4));
+    assert_eq!(set, [Some(0), Some(1), Some(2)]);
+    assert!(permutation(&mut set, 4));
+    assert_eq!(set, [Some(0), Some(1), Some(3)]);
+    assert!(permutation(&mut set, 4));
+    assert_eq!(set, [Some(0), Some(2), Some(3)]);
+
+    assert!(permutation(&mut set, 4));
+    assert_eq!(set, [Some(1), Some(2), Some(3)]);
+
+    assert!(!permutation(&mut set, 2));
+  }
 }
