bc7845426f388202c5f69180a0b2e52517fdaf4f
Luke Parker <lukeparker5132@gmail.com>
2026-07-24 22:29:35 -0400

    Perform MSRV checks against a minimal lockfile
    This considers each crate independently, taking advantage of
    `-Zminimal-versions` to find the minimal lockfiles. This also helps ensure the
    accuracy of the declared version bounds in each crate.
    
    To ensure the MSRV is considered _as relevant to publishing_,
    `[dev-dependencies]` are stripped from the `Cargo.toml` before deciding the
    lockfile.

--- orchestration/src/mimalloc.rs
+++ orchestration/src/mimalloc.rs
@@ -1,7 +1,7 @@
 use crate::Os;
 
-// 3.4.3
-const MIMALLOC_VERSION: &str = "152fbf2634aeafca3774df791b0a77683035f076";
+// 3.4.4
+const MIMALLOC_VERSION: &str = "1f06f694972279bbc7ec72902e8570f5784d0fc9";
 const HARDENING_FLAGS: &str = "-DMI_SECURE_FULL=ON -DMI_GUARDED=ON -DMI_XMALLOC=ON";
 #[rustfmt::skip]
 const COMPILATION_FLAGS: &str =
